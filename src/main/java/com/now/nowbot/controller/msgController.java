ba34b211b44f0bc36ffb8a06fb61c1289d4cfaa0
addaye <addaye@163.com>
2021-08-14 16:04:58 +0800

    修改完毕

--- src/main/java/com/now/nowbot/controller/msgController.java
+++ src/main/java/com/now/nowbot/controller/msgController.java
@@ -5,11 +5,13 @@ import com.now.nowbot.service.OsuGetService;
 import com.now.nowbot.service.msgServiceImpl.BindingServiceImpl;
 import com.now.nowbot.util.BindingUtil;
 import net.mamoe.mirai.Bot;
-import net.mamoe.mirai.message.data.MessageSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(produces = "application/json;charset=UTF-8")
@@ -26,23 +28,11 @@ public class msgController {
     @ResponseBody
     public Object request(@RequestParam(name = "code") String code, @RequestParam(name = "state")String state){
         String[] data = state.split(" ");
-        threadPool.execute(()->{
-            if(data.length != 0) {
-                BinUser bd = new BinUser(Long.valueOf(data[0]), code);
-                osuGetService.getToken(bd);
-                osuGetService.getPlayerInfo(bd);
-                BindingUtil.writeUser(bd);
-                BindingUtil.writeOsuID(bd.getOsuName(),bd.getOsuID());
-                if (data.length == 2){
-                    bot.getGroup(Long.valueOf(data[1])).sendMessage("成功绑定:" + bd.getQq() + "->" + bd.getOsuName());
-                }else
-                    bot.getFriend(Long.valueOf(data[0])).sendMessage("成功绑定:" + bd.getQq() + "->" + bd.getOsuName());
-            }
-        });
+        saveBind(code, data);
         return data[0]+"接收成功！";
     }
     @Async
-    public void callbackMsg(String code, String[] data){
+    public void saveBind(String code, String[] data){
         if(data.length == 2) {
             BinUser bd = new BinUser(Long.valueOf(data[0]), code);
             osuGetService.getToken(bd);
@@ -52,7 +42,7 @@ public class msgController {
             var msg = bin.msgs.get(Long.valueOf(data[1]));
             if (msg != null) {
                 msg.getTarget().sendMessage("成功绑定:" + bd.getQq() + "->" + bd.getOsuName());
-                MessageSource.recall(msg.getSource());
+                msg.recall();
             }
 
         }
