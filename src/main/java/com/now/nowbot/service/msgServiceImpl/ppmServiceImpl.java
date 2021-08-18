855caf2f5134dbbfa84b9010ce9500ec10ca8154
addaye <addaye@163.com>
2021-08-18 15:08:04 +0800

    1

--- src/main/java/com/now/nowbot/service/msgServiceImpl/ppmServiceImpl.java
+++ src/main/java/com/now/nowbot/service/msgServiceImpl/ppmServiceImpl.java
@@ -65,50 +65,50 @@ public class ppmServiceImpl extends MessageService{
             canvas.drawPath(SkiaUtil.creat6(250, 0, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f), bg1);
             canvas.drawPath(SkiaUtil.creat6(250, 0, 1, 1, 1, 1, 1, 1), bg1);
 
-            Path pt = SkiaUtil.creat6(250, 3,(float) userinfo.getPtt(),(float) userinfo.getSta(),(float) userinfo.getStb(),(float) userinfo.getSth(),(float) userinfo.getEng(),(float) userinfo.getFa());
+            Path pt = SkiaUtil.creat6(250, 3,(float)(userinfo.getPtt()-0.5)*2,(float) (userinfo.getSta()-0.5)*2,(float) (userinfo.getStb()-0.5)*2,(float) (userinfo.getSth()-0.5)*2,(float) (userinfo.getEng()-0.5)*2,(float) (userinfo.getFa()-0.5)*2);
 
             canvas.drawPath(pt, new Paint().setStrokeWidth(3).setStroke(true).setARGB(255,240,167,50));
             canvas.drawPath(pt, new Paint().setStrokeWidth(3).setStroke(false).setARGB(80, 240, 167, 50));
 
             canvas.drawRRect(RRect.makeXYWH(-150,-226.5f,60,25,5),bg2);
-            canvas.drawString("成长值",-144,-208f,smileFont,wp);
+            canvas.drawString("ptt",-144,-208f,smileFont,wp);
 
             canvas.drawRRect(RRect.makeXYWH(100,-226.5f,60,25,5),bg2);
-            canvas.drawString("持久力",108,-208.5f,smileFont,wp);
+            canvas.drawString("sta",108,-208.5f,smileFont,wp);
 
             canvas.drawRRect(RRect.makeXYWH(230,-10,50,25,5),bg2);
-            canvas.drawString("稳定性",239,7,smileFont,wp);
+            canvas.drawString("stb",239,7,smileFont,wp);
 
             canvas.drawRRect(RRect.makeXYWH(105,206.5f,50,25,5),bg2);
-            canvas.drawString("实力",114,223.5f,smileFont,wp);
+            canvas.drawString("sth",114,223.5f,smileFont,wp);
 
             canvas.drawRRect(RRect.makeXYWH(-145,206.5f,50,25,5),bg2);
-            canvas.drawString("努力值",-137,223.5f,smileFont,wp);
+            canvas.drawString("eng",-137,223.5f,smileFont,wp);
 
             canvas.drawRRect(RRect.makeXYWH(-270,-10,50,25,5),bg2);
-            canvas.drawString("精准度",-261,7f,smileFont,wp);
+            canvas.drawString("ffa",-261,7f,smileFont,wp);
 
             canvas.restore();
             canvas.translate(0,575);
 
             DecimalFormat dx = new DecimalFormat("0.00");
             canvas.drawRRect(RRect.makeXYWH(50,0,225,50,10),edP);
-            canvas.drawString("成长值:"+dx.format(userinfo.getPtt()*100),60,35,middleFont,wp);
+            canvas.drawString("PTT:"+dx.format(userinfo.getPtt()*100),60,35,middleFont,wp);
             canvas.drawRRect(RRect.makeXYWH(325,0,225,50,10),edP);
-            canvas.drawString("持久力:"+dx.format(userinfo.getStb()*100),335,35,middleFont,wp);
+            canvas.drawString("STA:"+dx.format(userinfo.getSta()*100),335,35,middleFont,wp);
 
             canvas.drawRRect(RRect.makeXYWH(50,75,225,50,10),edP);
-            canvas.drawString("稳定性:"+dx.format(userinfo.getStb()*100),60,110,middleFont,wp);
+            canvas.drawString("STB:"+dx.format(userinfo.getStb()*100),60,110,middleFont,wp);
             canvas.drawRRect(RRect.makeXYWH(325,75,225,50,10),edP);
-            canvas.drawString("实力:"+dx.format(userinfo.getFa()*100),335,110,middleFont,wp);
+            canvas.drawString("FFA:"+dx.format(userinfo.getFa()*100),335,110,middleFont,wp);
 
             canvas.drawRRect(RRect.makeXYWH(50,150,225,50,10),edP);
-            canvas.drawString("努力值:"+dx.format(userinfo.getSth()*100),60,185,middleFont,wp);
+            canvas.drawString("STH:"+dx.format(userinfo.getSth()*100),60,185,middleFont,wp);
             canvas.drawRRect(RRect.makeXYWH(325,150,225,50,10),edP);
-            canvas.drawString("精准度:"+dx.format(userinfo.getEng()*100),335,185,middleFont,wp);
+            canvas.drawString("ENG:"+dx.format(userinfo.getEng()*100),335,185,middleFont,wp);
 
-            var fromx = TextLine.make("感谢Muziyami提供计算方法",smileFont);
-            canvas.drawTextLine(fromx,(600-line.getWidth())/2,surface.getHeight()-line.getHeight(),new Paint().setARGB(255,255,255,255));
+//            var fromx = TextLine.make("Thanks for Muziyami",smileFont);
+//            canvas.drawTextLine(fromx,(600-line.getWidth())/2,surface.getHeight()-2*line.getHeight(),new Paint().setARGB(255,255,255,255));
 
             var datebyte = surface.makeImageSnapshot().encodeToData().getBytes();
             from.sendMessage(ExternalResource.uploadAsImage(ExternalResource.create(datebyte),from));
