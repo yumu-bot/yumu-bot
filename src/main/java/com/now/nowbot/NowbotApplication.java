068a58bb78ab13f6187c4422b089c88b57cfbfad
addaye <addaye@163.com>
2021-08-18 10:04:02 +0800

    update

--- src/main/java/com/now/nowbot/NowbotApplication.java
+++ src/main/java/com/now/nowbot/NowbotApplication.java
@@ -13,7 +13,8 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 public class NowbotApplication {
     public static void main(String[] args) {
         SpringApplication.run(NowbotApplication.class, args);
-
+        System.out.println("Max:"+Runtime.getRuntime().maxMemory()/1024/1024+'M');
+        System.out.println("Total:"+Runtime.getRuntime().totalMemory()/1024/1024+'M');
     }
 //    @Scheduled(cron = "0/5 * * * * *") 定时任务
 //    public void A(){
--- src/main/java/com/now/nowbot/service/msgServiceImpl/ppmServiceImpl.java
+++ src/main/java/com/now/nowbot/service/msgServiceImpl/ppmServiceImpl.java
@@ -32,85 +32,157 @@ public class ppmServiceImpl extends MessageService{
         if (user == null){
             from.sendMessage("您未绑定，请绑定后使用");
         }
-
-        var userdate = osuGetService.getPlayerOsuInfo(user);
-        var bpdate = osuGetService.getOsuBestMap(user,0,100);
-        dates userinfo= new dates(userdate, bpdate);
-
-        double fa = (userinfo.acc<0.6D?0:(userinfo.acc-0.6)*Math.pow(2.5D,1.776D));
-        double ptt;
-    }
-}
-class dates{
-    float ppv0=0;
-    float ppv45=0;
-    float ppv90=0;
-    float accv0=0;
-    float accv45=0;
-    float accv90=0;
-    long lengv0=0;
-    long lengv45=0;
-    long lengv90=0;
-    double bpp=0;
-    int xd=0;
-    int xc=0;
-    int xb=0;
-    int xa=0;
-    int xs=0;
-    int xx=0;
-    int notfc=0;
-    String name;
-    float pp ;
-    float acc;
-    int level;
-    int rank ;
-    int combo;
-    long thit;
-    long pcont;
-    long ptime;
-    dates(JSONObject prd, JSONArray prbp){
-
-        for (int j = 0; j < prbp.size(); j++) {
-            var jsb = prbp.getJSONObject(j);
-            bpp += jsb.getDoubleValue("pp")*Math.pow(0.95d,j);
-            if (jsb.getString("rank").startsWith("D")) xd++;
-            if (jsb.getString("rank").startsWith("C")) xc++;
-            if (jsb.getString("rank").startsWith("B")) xb++;
-            if (jsb.getString("rank").startsWith("A")) xa++;
-            if (jsb.getString("rank").startsWith("S")) xs++;
-            if (jsb.getString("rank").startsWith("X")) xx++;
-            if(!jsb.getBoolean("perfect")) notfc++;
-            if(j < 10){
-                ppv0 += jsb.getFloatValue("pp");
-                accv0 += jsb.getFloatValue("accuracy");
-                lengv0 += jsb.getJSONObject("beatmap").getFloatValue("total_length");
-            }else if(j>=45 && j<55){
-                ppv45 += jsb.getFloatValue("pp");
-                accv45 += jsb.getFloatValue("accuracy");
-                lengv45 += jsb.getJSONObject("beatmap").getFloatValue("total_length");
-            }else if(j>=90){
-                ppv90 += jsb.getFloatValue("pp");
-                accv90 += jsb.getFloatValue("accuracy");
-                lengv90 += jsb.getJSONObject("beatmap").getFloatValue("total_length");
-            }
+        dates userinfo;
+        {
+            var userdate = osuGetService.getPlayerOsuInfo(user);
+            var bpdate = osuGetService.getOsuBestMap(user, 0, 100);
+            userinfo = new dates(userdate, bpdate);
         }
-        ppv0 /= 10;
-        ppv45 /= 10;
-        ppv90 /= 10;
-        accv0 /= 10;
-        accv45 /= 10;
-        accv90 /= 10;
-        lengv0 /= 10;
-        lengv45 /= 10;
-        lengv90 /= 10;
-        name = prd.getString("username").replace(' ','_');
-        pp = prd.getJSONObject("statistics").getFloatValue("pp");
-        acc = prd.getJSONObject("statistics").getFloatValue("hit_accuracy");
-        level = prd.getJSONObject("statistics").getJSONObject("level").getIntValue("current");
-        rank = prd.getJSONObject("statistics").getIntValue("global_rank");
-        combo = prd.getJSONObject("statistics").getIntValue("maximum_combo");
-        thit = prd.getJSONObject("statistics").getLongValue("total_hits");
-        pcont = prd.getJSONObject("statistics").getLongValue("play_count");
-        ptime = prd.getJSONObject("statistics").getLongValue("play_time");
+
+        //1.1 准度fACC formulaic accuracy 0-1 fa
+        double fa = (userinfo.acc<0.6D?0:(userinfo.acc-0.6)*Math.pow(2.5D,1.776D));
+        //1.2 1.2 潜力PTT potential 0-1 ptt
+        double ptt;
+        {
+            double bpmxd = Math.pow(0.9D, userinfo.ppv45 / (userinfo.accv0 - userinfo.ppv90 + 1));
+            double rBPD = userinfo.rawpp / userinfo.ppv0;
+            double BPD;
+            if (rBPD <= 14) {
+                BPD = 1;
+            } else if (rBPD <= 18) {
+                BPD = (18 - rBPD) * 0.1D + 0.6D;
+            } else if (rBPD <= 19) {
+                BPD = (19 - rBPD) * 0.6D;
+            } else {
+                BPD = 0;
+            }
+            ptt = Math.pow((BPD*0.4 + bpmxd*0.2 + 0.4),0.8D);
+        }
+        //1.3 耐力STA stamina 0-1.2 sta
+        double sta;
+        {
+            double rSP = 1.0*userinfo.ptime/userinfo.pcont;
+            double SPT;
+            if(rSP<30){
+                SPT = 0;
+            }else if(rSP<=180){
+                SPT = 1 - Math.pow((180-rSP)/150, 2.357);
+            }else{
+                SPT = 1;
+            }
+            double rLN = userinfo.lengv0*0.7 + userinfo.lengv45*0.2 + userinfo.lengv90*0.1;
+            double VLB;
+            if(rLN<180){
+                VLB = 0;
+            }else if(rLN<=240){
+                VLB = Math.pow((rLN-180)/60,0.4);
+            }else{
+                VLB = 1;
+            }
+            sta = Math.pow(SPT,0.8D) + VLB * 0.2;
+        }
+        //1.4 稳定STB stability (-0.16)-1.2 stb
+//        double
+    }
+    static class dates{
+        float ppv0=0;
+        float ppv45=0;
+        float ppv90=0;
+        float accv0=0;
+        float accv45=0;
+        float accv90=0;
+        long lengv0=0;
+        long lengv45=0;
+        long lengv90=0;
+        double bpp=0;
+        double rawpp = 0;
+        int xd=0;
+        int xc=0;
+        int xb=0;
+        int xa=0;
+        int xs=0;
+        int xx=0;
+        int notfc=0;
+        String name;
+        float pp ;
+        float acc;
+        int level;
+        int rank ;
+        int combo;
+        long thit;
+        long pcont;
+        long ptime;
+        dates(JSONObject prd, JSONArray prbp){
+
+            double[] ys = new double[prbp.size()];
+            for (int j = 0; j < prbp.size(); j++) {
+                var jsb = prbp.getJSONObject(j);
+                bpp += jsb.getDoubleValue("pp")*Math.pow(0.95d,j);
+                ys[j] = Math.log10(jsb.getDoubleValue("pp") * Math.pow(0.95, j)) / Math.log10(100);
+
+                if (jsb.getString("rank").startsWith("D")) xd++;
+                if (jsb.getString("rank").startsWith("C")) xc++;
+                if (jsb.getString("rank").startsWith("B")) xb++;
+                if (jsb.getString("rank").startsWith("A")) xa++;
+                if (jsb.getString("rank").startsWith("S")) xs++;
+                if (jsb.getString("rank").startsWith("X")) xx++;
+                if(!jsb.getBoolean("perfect")) notfc++;
+                if(j < 10){
+                    ppv0 += jsb.getFloatValue("pp");
+                    accv0 += jsb.getFloatValue("accuracy");
+                    lengv0 += jsb.getJSONObject("beatmap").getFloatValue("total_length");
+                }else if(j>=45 && j<55){
+                    ppv45 += jsb.getFloatValue("pp");
+                    accv45 += jsb.getFloatValue("accuracy");
+                    lengv45 += jsb.getJSONObject("beatmap").getFloatValue("total_length");
+                }else if(j>=90){
+                    ppv90 += jsb.getFloatValue("pp");
+                    accv90 += jsb.getFloatValue("accuracy");
+                    lengv90 += jsb.getJSONObject("beatmap").getFloatValue("total_length");
+                }
+            }
+            double sumOxy = 0.0D;
+            double sumOx2 = 0.0D;
+            double avgX = 0.0D;
+            double avgY = 0.0D;
+            double sumX = 0.0D;
+            for(int n = 1; n <= ys.length; n++){
+                double weight = Math.log1p(n + 1.0D);
+                sumX += weight;
+                avgX += n * weight;
+                avgY += ys[n - 1] * weight;
+            }
+            avgX /= sumX;
+            avgY /= sumX;
+            for(int n = 1; n <= ys.length; n++){
+                sumOxy += (n - avgX) * (ys[n - 1] - avgY) * Math.log1p(n + 1.0D);
+                sumOx2 += Math.pow(n - avgX, 2.0D) * Math.log1p(n + 1.0D);
+            }
+            double Oxy = sumOxy / sumX;
+            double Ox2 = sumOx2 / sumX;
+            for(double n = 100; n <= prd.getJSONObject("statistics").getIntValue("play_count"); n++){
+                double val = Math.pow(100.0D, (avgY - (Oxy / Ox2) * avgX) + (Oxy / Ox2) * n);
+                rawpp = bpp+val;
+            }
+
+            ppv0 /= 10;
+            ppv45 /= 10;
+            ppv90 /= 10;
+            accv0 /= 10;
+            accv45 /= 10;
+            accv90 /= 10;
+            lengv0 /= 10;
+            lengv45 /= 10;
+            lengv90 /= 10;
+            name = prd.getString("username").replace(' ','_');
+            pp = prd.getJSONObject("statistics").getFloatValue("pp");
+            acc = prd.getJSONObject("statistics").getFloatValue("hit_accuracy");
+            level = prd.getJSONObject("statistics").getJSONObject("level").getIntValue("current");
+            rank = prd.getJSONObject("statistics").getIntValue("global_rank");
+            combo = prd.getJSONObject("statistics").getIntValue("maximum_combo");
+            thit = prd.getJSONObject("statistics").getLongValue("total_hits");
+            pcont = prd.getJSONObject("statistics").getLongValue("play_count");
+            ptime = prd.getJSONObject("statistics").getLongValue("play_time");
+        }
     }
 }
