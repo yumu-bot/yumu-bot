3bbe89e52ae96cbd21158372868c9d9cd8b2910f
addaye <addaye@163.com>
2021-08-22 21:07:29 +0800

    update

--- src/main/java/com/now/nowbot/service/MessageService/catpanelService.java
+++ src/main/java/com/now/nowbot/service/MessageService/catpanelService.java
@@ -17,9 +17,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Service("cpanel")
-public class catpanelService extends MsgSTemp implements MessageService{
-    catpanelService(){
-        super(Pattern.compile("[!！]\\s*(?i)cpanel(\\s+bk:(?<bk>\\d+))?(\\s+?<yl>ylbx)?"),"cpanel");
+public class catpanelService extends MsgSTemp implements MessageService {
+    catpanelService() {
+        super(Pattern.compile("[!！]\\s*(?i)cpanel(\\s+bk:(?<bk>\\d+))?(\\s+?<yl>ylbx)?"), "cpanel");
     }
 
     @Override
@@ -27,10 +27,10 @@ public class catpanelService extends MsgSTemp implements MessageService{
         var from = event.getSubject();
         var rep = event.getMessage().get(QuoteReply.Key);
         Image img = null;
-        if (rep != null){
-            img = (Image)rep.getSource().getOriginalMessage().stream().filter(i->i instanceof Image).findFirst().orElse(null);
-        }else {
-            img = (Image)event.getMessage().stream().filter(i->i instanceof Image).findFirst().orElse(null);
+        if (rep != null) {
+            img = (Image) rep.getSource().getOriginalMessage().stream().filter(i -> i instanceof Image).findFirst().orElse(null);
+        } else {
+            img = (Image) event.getMessage().stream().filter(i -> i instanceof Image).findFirst().orElse(null);
         }
         if (img == null) {
             from.sendMessage("没有图片！");
@@ -39,53 +39,53 @@ public class catpanelService extends MsgSTemp implements MessageService{
         boolean stl = false;
         int tmd = 0;
         while (matcher.find()) {
-            if(matcher.group("bk") != null){
+            if (matcher.group("bk") != null) {
                 try {
                     tmd = Integer.parseInt(matcher.group("bk"));
-                    if (tmd<0 || tmd>100) throw new Exception("数值不合法");
+                    if (tmd < 0 || tmd > 100) throw new Exception("数值不合法");
                 } catch (Exception e) {
                     from.sendMessage(e.getMessage());
                     return;
                 }
             }
-            if(matcher.group("yl") != null) stl = true;
+            if (matcher.group("yl") != null) stl = true;
 
-            org.jetbrains.skija.Image netImg = null;
-            org.jetbrains.skija.Image topImg = null;
-
-            netImg = SkiaUtil.lodeNetWorkImage(Image.queryUrl(img));
-            topImg = SkiaUtil.fileToImage(NowbotConfig.BG_PATH +"panel05.png");
-
-            int imgWidth = netImg.getWidth();
-            int imgHeight = netImg.getHeight();
-
-            org.jetbrains.skija.Image siImg;
-            if(1f*imgWidth/imgHeight<1200f/857) {
-                org.jetbrains.skija.Image img1 = SkiaUtil.getScaleImage(netImg, 1200, 1200 * imgHeight / imgWidth);
-                siImg = SkiaUtil.getCutImage(img1, 0, (img1.getHeight()-857)/2, 1200, 857);
-            }else {
-                org.jetbrains.skija.Image img1 = SkiaUtil.getScaleImage(netImg, 857*imgWidth/imgHeight, 857);
-                siImg = SkiaUtil.getCutImage(img1, (img1.getWidth()-1200)/2, 0, 1200, 857);
-            }
-            byte[] pngBytes;
-            try (Surface surface = Surface.makeRasterN32Premul(1200, 857);){
-                Canvas canvas = surface.getCanvas();
-                canvas.drawImage(siImg,0,0);
-                canvas.drawRect(Rect.makeXYWH(0,0,surface.getWidth(),surface.getHeight()),new Paint().setARGB(255*tmd/100,0,0,0));
-                canvas.drawImage(topImg,0,0);
-                try {
-                    if (stl){
-                        canvas.drawImage(SkiaUtil.fileToImage(NowbotConfig.BG_PATH+"ylbx.png"),0,0);
-                    }else {
-                        canvas.drawImage(SkiaUtil.fileToImage(NowbotConfig.BG_PATH+"lbx.png"),0,0);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                pngBytes = surface.makeImageSnapshot().encodeToData().getBytes();
-
-            }
-            from.sendMessage(ExternalResource.uploadAsImage(ExternalResource.create(pngBytes), from));
         }
+        org.jetbrains.skija.Image netImg = null;
+        org.jetbrains.skija.Image topImg = null;
+
+        netImg = SkiaUtil.lodeNetWorkImage(Image.queryUrl(img));
+        topImg = SkiaUtil.fileToImage(NowbotConfig.BG_PATH + "panel05.png");
+
+        int imgWidth = netImg.getWidth();
+        int imgHeight = netImg.getHeight();
+
+        org.jetbrains.skija.Image siImg;
+        if (1f * imgWidth / imgHeight < 1200f / 857) {
+            org.jetbrains.skija.Image img1 = SkiaUtil.getScaleImage(netImg, 1200, 1200 * imgHeight / imgWidth);
+            siImg = SkiaUtil.getCutImage(img1, 0, (img1.getHeight() - 857) / 2, 1200, 857);
+        } else {
+            org.jetbrains.skija.Image img1 = SkiaUtil.getScaleImage(netImg, 857 * imgWidth / imgHeight, 857);
+            siImg = SkiaUtil.getCutImage(img1, (img1.getWidth() - 1200) / 2, 0, 1200, 857);
+        }
+        byte[] pngBytes;
+        try (Surface surface = Surface.makeRasterN32Premul(1200, 857);) {
+            Canvas canvas = surface.getCanvas();
+            canvas.drawImage(siImg, 0, 0);
+            canvas.drawRect(Rect.makeXYWH(0, 0, surface.getWidth(), surface.getHeight()), new Paint().setARGB(255 * tmd / 100, 0, 0, 0));
+            canvas.drawImage(topImg, 0, 0);
+            try {
+                if (stl) {
+                    canvas.drawImage(SkiaUtil.fileToImage(NowbotConfig.BG_PATH + "ylbx.png"), 0, 0);
+                } else {
+                    canvas.drawImage(SkiaUtil.fileToImage(NowbotConfig.BG_PATH + "lbx.png"), 0, 0);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            pngBytes = surface.makeImageSnapshot().encodeToData().getBytes();
+
+        }
+        from.sendMessage(ExternalResource.uploadAsImage(ExternalResource.create(pngBytes), from));
     }
 }
