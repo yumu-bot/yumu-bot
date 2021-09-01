7f41fc2c0f6e44ad67868db6fad705b97b5db4f4
spring_night <2480557535@qq.com>
2021-09-01 14:57:25 +0800

    添加定时器

---
+++ src/main/java/com/now/nowbot/service/RunTimeService.java
@@ -0,0 +1,36 @@
+package com.now.nowbot.service;
+
+import com.now.nowbot.config.AsyncSetting;
+import net.mamoe.mirai.Bot;
+import net.mamoe.mirai.message.data.At;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RunTimeService {
+    private static final Logger log = LoggerFactory.getLogger(RunTimeService.class);
+    @Autowired
+    Bot bot;
+    @Scheduled(cron = "0 0 0 * * *")
+    public void sleep(){
+        bot.getGroups().forEach(group -> {
+            var gs = group.getMembers();
+            try {
+                var n = gs.getOrFail(1529813731L);
+                group.sendMessage(new At(n.getId()).plus("快去睡啦"));
+            } catch (Exception e) {
+            }
+        });
+    }
+    @Scheduled(cron = "0 0/30 8-18 * * *")
+    public void alive(){
+        bot.getGroup(746671531L).sendMessage("定时任务测试\n0 0/30 8-18 * * * ?");
+    }
+    @Scheduled(cron = "0/10 * 8-18 * * *")
+    public void test(){
+        log.info("time:{}",System.currentTimeMillis());
+    }
+}
