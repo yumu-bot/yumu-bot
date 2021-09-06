fcbab3b8887c10dd50020e3c6c2d42b958c97e42
aaa bbb <2480557535@qq.com>
2021-09-06 16:15:05 +0000

    227 fa->facc

--- src/main/java/com/now/nowbot/entity/PPm/PPmCatch.java
+++ src/main/java/com/now/nowbot/entity/PPm/PPmCatch.java
@@ -224,7 +224,7 @@ public class PPmCatch implements PPmObject {
             sth = Math.pow((HPS-5)/4,0.2);
             sth = check(sth, 0, 1);
         }
-        ttl = fa*0.2 + ptt*0.1 + eng*0.1 + sth*0.2 + stb*0.2 + sta*0.2;
+        ttl = facc*0.2 + ptt*0.1 + eng*0.1 + sth*0.2 + stb*0.2 + sta*0.2;
         san = 0;
         // ppmcatch san没写好uwu
     }
