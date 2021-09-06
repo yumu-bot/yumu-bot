194dd54277431ed26e41e0f0666ed6e5f67edde6
spring_night <2480557535@qq.com>
2021-09-06 11:02:36 +0800

    啥b autowrite

--- src/main/java/com/now/nowbot/controller/msgController.java
+++ src/main/java/com/now/nowbot/controller/msgController.java
@@ -1,5 +1,6 @@
 package com.now.nowbot.controller;
 
+import com.now.nowbot.config.NowbotConfig;
 import com.now.nowbot.entity.BinUser;
 import com.now.nowbot.service.MessageService.bindService;
 import com.now.nowbot.service.OsuGetService;
@@ -17,11 +18,6 @@ public class msgController {
     bindService bin;
 
     @Autowired
-    public void setBin(bindService service) {
-        bin = service;
-    }
-
-    @Autowired
     OsuGetService osuGetService;
     @Autowired
     ThreadPoolTaskExecutor threadPool;
@@ -34,7 +30,9 @@ public class msgController {
     }
 
     public String saveBind(String code, String[] data) {
-        if (bin != null)
+        if (bin == null) {
+            bin = NowbotConfig.applicationContext.getBean("bind", bindService.class);
+        }
             if (data.length == 2) {
 
                 var msg = bin.msgs.get(Long.valueOf(data[1]));
@@ -42,7 +40,7 @@ public class msgController {
                 if (msg != null) {
                     try {
                         msg.recall();
-                        BinUser bd = new BinUser(Long.valueOf(data[0]), code);
+                        BinUser bd = new BinUser(Long.parseLong(data[0]), code);
                         osuGetService.getToken(bd);
                         osuGetService.getPlayerOsuInfo(bd);
                         BindingUtil.writeUser(bd);
@@ -51,13 +49,14 @@ public class msgController {
                         bin.msgs.remove(Long.valueOf(data[1]));
                         return "成功绑定:" + bd.getQq() + "->" + bd.getOsuName();
                     } catch (Exception e) {
-                        return "出现异常,请截图给开发者让他抓紧修bug\n"+e.getLocalizedMessage();
+                        return "出现异常,请截图给开发者让他抓紧修bug\n" + e.getLocalizedMessage();
                     }
-                }else {
+                } else {
                     return "绑定链接已失效,请重新绑定,请勿重复绑定";
                 }
 
             }
-        return "别试了,没用的";
+            return "别试了,没用的";
+
     }
 }
