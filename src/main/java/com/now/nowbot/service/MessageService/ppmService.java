159ecdfd25a77ed9eb57bf93c0abd369f7e0e6e8
aaa bbb <2480557535@qq.com>
2021-09-06 17:34:33 +0000

    116 delete }

--- src/main/java/com/now/nowbot/service/MessageService/ppmService.java
+++ src/main/java/com/now/nowbot/service/MessageService/ppmService.java
@@ -113,7 +113,6 @@ public class ppmService extends MsgSTemp implements MessageService {
             }
         }
         if (userinfo == null) throw new TipsException("「邪恶的 osu! 玩家，我以 Bot 一族」…呃，这里不会读…「Bot 大魔王之名，否定你添加新模式的资格！」「除非你干掉 peppy，通过」…呃…「接受」…呃… 有几个词，波特不认识…");
-        }
         if (userinfo.getPtime()<60 || userinfo.getPcont()<30){
             throw new TipsException("游戏时长太短了，快去多玩几局吧！");
         }
