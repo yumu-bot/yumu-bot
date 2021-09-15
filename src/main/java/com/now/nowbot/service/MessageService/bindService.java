ca6d7601fe5887c9b22be96db9cf1412253b99bc
aaa bbb <2480557535@qq.com>
2021-09-15 15:46:51 +0000

    Update bindService.java

--- src/main/java/com/now/nowbot/service/MessageService/bindService.java
+++ src/main/java/com/now/nowbot/service/MessageService/bindService.java
@@ -38,7 +38,7 @@ public class bindService implements MessageService {
 
     @Override
     public void HandleMessage(MessageEvent event, Matcher matcher) throws Throwable {
-        if (event != null ) throw new TipsException("现在停止绑定,查询功能请使用相应的指令+osu名");
+        //if (event != null ) throw new TipsException("现在停止绑定,查询功能请使用相应的指令+osu名");
         //将当前毫秒时间戳作为 key
         long d = System.currentTimeMillis();
         //群聊验证是否绑定
