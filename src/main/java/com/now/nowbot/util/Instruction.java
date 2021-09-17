45a1053e94fea2ce5d92cddefaf2688359dcac16
spring_night <2480557535@qq.com>
2021-09-17 19:20:01 +0800

    匹配

--- src/main/java/com/now/nowbot/util/Instruction.java
+++ src/main/java/com/now/nowbot/util/Instruction.java
@@ -14,15 +14,15 @@ public enum Instruction {
     HELP("help", null, "^[!！](?i)ymhelp$"),
     PAGE("page", null, "^[!！]page(\\s+(?<p1>\\w+))?(\\s?(?<p2>\\w+))?(\\s?(?<p3>\\w+))?"),
     PING("ping", "网络测试", "^[!！]\\s?(?i)ymping"),
-    PPM("ppm", "某种实力计算方法", "^[!！]\\s?(?i)ymppm(?!v)([:：](?<mode>[\\w\\d]+))?(\\s+(?<name>[0-9a-zA-Z\\[\\]\\-_ ]*))?"),
+    PPM("ppm", "某种实力计算方法", "^[!！]\\s?(?i)ymppm(?![vV])([:：](?<mode>[\\w\\d]+))?(\\s+(?<name>[0-9a-zA-Z\\[\\]\\-_ ]*))?"),
     PPMVS("ppmvs", "PPM对比", "^[!！]\\s?(?i)ymppmvs([:：](?<mode>[\\w\\d]+))?(\\s+(?<name>[0-9a-zA-Z\\[\\]\\-_ ]*))?"),
-    PPPLUS("ppp", "另一种pp计算方法", "^[!！]\\s*(?i)ppp(?![v])(\\s+(?<name>[0-9a-zA-Z\\[\\]\\-_ ]*))?"),
+    PPPLUS("ppp", "另一种pp计算方法", "^[!！]\\s*(?i)ppp(?![vV])(\\s+(?<name>[0-9a-zA-Z\\[\\]\\-_ ]*))?"),
     PPPLUSVS("ppvs", "ppp对比", "^[!！]\\s?(?i)p([pP]*)?vs(\\s*(?<name>[0-9a-zA-Z\\[\\]\\-_ ]*))?"),
     SETU("setu", "获取色图", "^[!！]((色图)|(涩图)|(setu))"),
     SONG("song", "获取谱面预览音频", "^[!！]\\s*csong\\s+(((sid[:=](?<sid>\\d+))|(bid[:=](?<bid>\\d+)))|(?<id>\\d+))"),
     START("start", "刷新积分", "^[!！]\\s*((积分)|(..积分))+"),
     KUMO("kumo", null, ".*"),
-    YMP("ymp",null,"^[!！](?i)ym(?<isAll>[p,r])([:：](?<mode>[\\w\\d]+))?(?!\\w*)(\\s+(?<name>[0-9a-zA-Z\\[\\]\\-_ ]*))?"),
+    YMP("ymp",null,"^[!！](?i)ym(?<isAll>[p,r])([:：](?<mode>[\\w\\d]+))?(?![pP])(\\s+(?<name>[0-9a-zA-Z\\[\\]\\-_ ]*))?"),
     WIKI("wiki",null,"^[!！](?i)ym((wiki)|w)(\\s+(?<key>[^\\s]*))?");
 
     private final Pattern regex;
