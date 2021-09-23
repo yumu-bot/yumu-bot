ee73e7ba3866cb0d2728af4b70b5c3505090d905
aaa bbb <2480557535@qq.com>
2021-09-23 17:55:26 +0000

    Replace Ymi.java

--- src/main/java/com/now/nowbot/model/Ymi.java
+++ src/main/java/com/now/nowbot/model/Ymi.java
@@ -29,8 +29,7 @@ public class Ymi {
     int grade_count_sh;
     int grade_count_a;
     int uid;
-    
-	public String getUrl(){return url;}
+
     public Ymi(JSONObject date){
         var user = date.getJSONObject("user");
         name = user.getString("username");
