52b69e58b4788a7162edeb5843c02754282d7b7e
spring_night <2480557535@qq.com>
2021-09-28 15:23:06 +0800

    注释jap相关代码

--- src/main/java/com/now/nowbot/mapper/UserMapper.java
+++ src/main/java/com/now/nowbot/mapper/UserMapper.java
@@ -1,7 +1,8 @@
 package com.now.nowbot.mapper;
 
-import com.now.nowbot.entity.UserLite;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserMapper extends JpaRepository<UserLite, Long> {
+
+public interface UserMapper
+//        extends JpaRepository<UserLite, Long>
+{
 }
