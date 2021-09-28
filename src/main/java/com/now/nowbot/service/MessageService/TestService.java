8198674e5228eab46b378fe2cb0414e2e7b50613
spring_night <2480557535@qq.com>
2021-09-28 14:54:38 +0800

    roll

--- src/main/java/com/now/nowbot/service/MessageService/TestService.java
+++ src/main/java/com/now/nowbot/service/MessageService/TestService.java
@@ -6,6 +6,8 @@ import com.now.nowbot.service.OsuGetService;
 import com.now.nowbot.util.BindingUtil;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
 import net.mamoe.mirai.event.events.MessageEvent;
+import net.mamoe.mirai.message.data.At;
+import net.mamoe.mirai.message.data.PlainText;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,10 +21,9 @@ public class TestService implements MessageService {
 
     @Override
     public void HandleMessage(MessageEvent event, Matcher aaa) throws Throwable {
+        var msg = event.getMessage();
         if (event instanceof GroupMessageEvent && ((GroupMessageEvent) event).getGroup().getId() == 746671531L) {
-            var msg = event.getMessage();
             var grp = ((GroupMessageEvent) event).getGroup();
-
             var pt = Pattern.compile("!testppm([:：](?<mode>[\\w\\d]+))?(\\s+(?<name>[0-9a-zA-Z\\[\\]\\-_ ]*))");
             var matcher = pt.matcher(msg.contentToString());
             if (matcher.find()) {
@@ -138,39 +139,19 @@ public class TestService implements MessageService {
                         .append(userinfo.getPpv90()).append(' ')
                         .append(userinfo.getAccv90()).append(' ')
                         .append(userinfo.getLengv90()).append(' ');
-//                sb.append("name").append(' ').append(userinfo.getName()).append('\n');
-//                sb.append("rank").append(' ').append(userinfo.getRank()).append('\n');
-//                sb.append("PP").append(' ').append(userinfo.getPp()).append('\n');
-//                sb.append("acc").append(' ').append(userinfo.getAcc()).append('\n');
-//                sb.append("level").append(' ').append(userinfo.getLevel()).append('\n');
-//                sb.append("max_cb").append(' ').append(userinfo.getCombo()).append('\n');
-//                sb.append("tth").append(' ').append(userinfo.getThit()).append('\n');
-//                sb.append("pc").append(' ').append(userinfo.getPcont()).append('\n');
-//                sb.append("time").append(' ').append(userinfo.getPtime()).append('\n');
-//                sb.append("no s rank:").append(' ').append(userinfo.getNotfc()).append('\n');
-//                sb.append("rowPP:").append(' ').append(userinfo.getRawpp()).append('\n');
-//                sb.append("ss数:").append(' ').append(userinfo.getXx()).append('\n');
-//                sb.append("s数:").append(' ').append(userinfo.getXs()).append('\n');
-//                sb.append("a数:").append(' ').append(userinfo.getXa()).append('\n');
-//                sb.append("b数:").append(' ').append(userinfo.getXb()).append('\n');
-//                sb.append("c数:").append(' ').append(userinfo.getXc()).append('\n');
-//                sb.append("d数:").append(' ').append(userinfo.getXd()).append('\n');
-//                sb.append("前10:\n");
-//                sb.append("pp").append(' ').append(userinfo.getPpv0()).append('\n');
-//                sb.append("acc").append(' ').append(userinfo.getAccv0()).append('\n');
-//                sb.append("length").append(' ').append(userinfo.getLengv0()).append('\n');
-//                sb.append("45:\n");
-//                sb.append("pp").append(' ').append(userinfo.getPpv45()).append('\n');
-//                sb.append("acc").append(' ').append(userinfo.getAccv45()).append('\n');
-//                sb.append("length").append(' ').append(userinfo.getLengv45()).append('\n');
-//                sb.append("90:\n");
-//                sb.append("pp").append(' ').append(userinfo.getPpv90()).append('\n');
-//                sb.append("acc").append(' ').append(userinfo.getAccv90()).append('\n');
-//                sb.append("length").append(' ').append(userinfo.getLengv90()).append('\n');
 
                 grp.sendMessage(sb.toString());
             }
+            return;
         }
-
+        var pt = Pattern.compile("^[!！]roll(\\s+(?<num>[0-9]{1,5}))?");
+        var matcher = pt.matcher(msg.contentToString());
+        if (matcher.find()){
+            if (matcher.group("num") != null){
+                event.getSubject().sendMessage(String.valueOf(1 + (System.currentTimeMillis() % Integer.parseInt(matcher.group("num")))));
+            }else {
+                event.getSubject().sendMessage(String.valueOf(1 + (System.currentTimeMillis() % 100)));
+            }
+        }
     }
 }
