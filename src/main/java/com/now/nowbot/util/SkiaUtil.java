83bf3e09a7c0eae8f672b3f32cfbb4d3d1d1652f
spring_night <2480557535@qq.com>
2021-10-25 14:43:00 +0800

    推送资源

--- src/main/java/com/now/nowbot/util/SkiaUtil.java
+++ src/main/java/com/now/nowbot/util/SkiaUtil.java
@@ -26,6 +26,8 @@ public class SkiaUtil {
     public static Typeface getTorusRegular(){
         if(TORUS_REGULAR == null || TORUS_REGULAR.isClosed()){
             try {
+//                InputStream in = SkiaUtil.class.getClassLoader().getResourceAsStream("static/font/Torus-Regular.ttf");
+//                TORUS_REGULAR = Typeface.makeFromData(Data.makeFromBytes(in.readAllBytes()));
                 TORUS_REGULAR = Typeface.makeFromFile(NowbotConfig.FONT_PATH + "Torus-Regular.ttf");
             } catch (Exception e) {
                 log.error("未读取到目标字体:Torus-Regular.ttf",e);
