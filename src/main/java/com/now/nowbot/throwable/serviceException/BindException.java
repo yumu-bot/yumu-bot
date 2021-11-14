793b7be8304edad72ac2ab0a03aec9492e27f295
spring_night <2480557535@qq.com>
2021-11-14 18:07:49 +0800

    提示格式

--- src/main/java/com/now/nowbot/throwable/serviceException/BindException.java
+++
@@ -1,7 +0,0 @@
-package com.now.nowbot.throwable.serviceException;
-
-import com.now.nowbot.throwable.TipsException;
-
-public class BindException extends TipsException {
-
-}
--- src/main/java/com/now/nowbot/throwable/serviceException/PpmException.java
+++ src/main/java/com/now/nowbot/throwable/serviceException/PpmException.java
@@ -2,44 +2,23 @@ package com.now.nowbot.throwable.serviceException;
 
 import com.now.nowbot.throwable.TipsException;
 
-import java.util.Random;
-
+/**
+ * ppm可能发生的异常
+ */
 public class PpmException extends TipsException {
-    public enum Type{
-        mode_mania(),
-        mode_not_find(),
-        time_short(),
-        unknown(),
-        ;
-
-    }
-    public PpmException(Type type){
-        switch (type){
-            case mode_mania:{
-                setMessage("等哪天mania社区风气变好了，或许就有PPM-mania了吧...");
-                break;
-            }
-            case mode_not_find:{
-                setMessage("「邪恶的 osu! 玩家，我以 Bot 一族」…呃，这里不会读…「Bot 大魔王之名，否定你添加新模式的资格！」「除非你干掉 peppy，通过」…呃…「接受」…呃… 有几个词，波特不认识…");
-                break;
-            }
-            case time_short:{
-                setMessage("游戏时长太短了，快去多玩几局吧！");
-                break;
-            }
-            case unknown: {
-                int flag = new Random().nextInt(3);
-                if (flag == 0) {
-                    setMessage("发生了某种不好的事情...");
-                }
-                if (flag == 1) {
-                    setMessage("发生了某种不好的事情...");
-                }
-                if (flag == 2) {
-                    setMessage("发生了某种不好的事情...");
-                }
-                break;
-            }
+    public static enum Exception{
+        异常名1("提示内容"),
+        异常名2("提示内容"),
+        ;//逗号分隔
+        String message;
+        Exception(String message) {
+            this.message = message;
         }
+    }
+    PpmException(String m){
+        super(m);
+    }
+    static PpmException getInstance(Exception type){
+        return new PpmException(type.message);
     }
 }
