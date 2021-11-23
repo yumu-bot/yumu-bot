5eb48895e07c337378a7f48519e5c7a2f916a1f4
spring_night <2480557535@qq.com>
2021-11-23 13:46:09 +0800

    help修改

--- src/main/java/com/now/nowbot/service/MessageService/HelpService.java
+++ src/main/java/com/now/nowbot/service/MessageService/HelpService.java
@@ -1,9 +1,12 @@
 package com.now.nowbot.service.MessageService;
 
-import com.now.nowbot.util.Instruction;
+import com.now.nowbot.config.NowbotConfig;
 import net.mamoe.mirai.event.events.MessageEvent;
+import net.mamoe.mirai.utils.ExternalResource;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.regex.Matcher;
 
 @Service("help")
@@ -11,11 +14,12 @@ public class HelpService implements MessageService {
     @Override
     public void HandleMessage(MessageEvent event, Matcher matcher) throws Throwable {
         var from = event.getSubject();
-        StringBuilder sb = new StringBuilder();
-        for(var ins: Instruction.values()) {
-            if(ins.getDesc()!=null)
-                sb.append(ins.getDesc()).append("\n");
-        }
-        from.sendMessage(sb.toString());
+//        StringBuilder sb = new StringBuilder();
+//        for(var ins: Instruction.values()) {
+//            if(ins.getDesc()!=null)
+//                sb.append(ins.getDesc()).append("\n");
+//        }
+//        from.sendMessage(sb.toString());
+        from.uploadImage(ExternalResource.create(Files.readAllBytes(Path.of(NowbotConfig.BG_PATH).resolve("ExportFileV3/panel-help.png"))));
     }
 }
