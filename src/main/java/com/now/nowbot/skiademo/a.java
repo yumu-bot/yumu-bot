25c61ed28251fdcccea9e0c34b8bcc5bd15da9d0
spring_night <2480557535@qq.com>
2022-01-28 16:39:52 +0800

    提交一个demo

---
+++ src/main/java/com/now/nowbot/skiademo/a.java
@@ -0,0 +1,63 @@
+package com.now.nowbot.skiademo;
+
+import com.now.nowbot.util.SkiaUtil;
+import org.jetbrains.skija.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class a {
+    public static void main(String[] args) {
+
+        int width = 800;
+        int height = 600;
+        //创建画布
+        Surface surface = Surface.makeRasterN32Premul(width, height);
+        //得到画纸,也可以叫画笔
+        Canvas canvas = surface.getCanvas();
+        //清空/重置画面
+        canvas.clear(Color.makeRGB(255,255,255));
+
+        //绘制图片
+        Image head = SkiaUtil.lodeNetWorkImage("https://a.ppy.sh/17064371?1622380408.jpeg"); //加载网图
+//        canvas.drawImage(head, 0, 0, new Paint());
+        canvas.drawImage(
+                head,       //图
+                0,     //x偏移
+                0    //y
+        );
+
+        //移动笔坐边
+        canvas.save(); //注意,save只会记录 坐标,缩放
+        canvas.translate(256,0);
+        canvas.drawImage(head, 0, 0); //画笔偏移 x:256
+        canvas.drawImage(head, 256, 0); //在画笔偏移基础上,再偏移x:256 y:0
+        canvas.restore();// 坐标,缩放 恢复到上次save
+
+        canvas.save();
+        canvas.translate(0, 256);
+//        图片缩放
+        Image mImage = SkiaUtil.getScaleImage(head, 100,100);
+        canvas.drawImage(mImage, 0,0);
+        //图片裁剪
+        Image cutImage = SkiaUtil.getCutImage(head, 0, 0, 100, 100);
+        canvas.drawImage(cutImage, 100,0);
+        //文字
+        Typeface typeface = Typeface.makeDefault();
+        Font font = new Font(typeface, 30);
+        TextLine line = TextLine.make("字",font);
+        canvas.drawTextLine(line,0,line.getCapHeight()+30*0.2f,new Paint().setARGB(100,255,0,255));
+
+        canvas.restore();
+        //输出
+
+        try {
+            Files.write(Path.of("D:/out.png"),
+                    surface.makeImageSnapshot().encodeToData(EncodedImageFormat.PNG).getBytes()
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
