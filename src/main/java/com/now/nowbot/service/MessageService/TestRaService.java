bd87508178a1da15a170a5916a06a3230e9900c1
Your Name <azxcvbzxc@foxmail.com>
2022-04-17 16:23:37 +0800

    testra 修改

--- src/main/java/com/now/nowbot/service/MessageService/TestRaService.java
+++ src/main/java/com/now/nowbot/service/MessageService/TestRaService.java
@@ -1,7 +1,7 @@
 package com.now.nowbot.service.MessageService;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.now.nowbot.aop.CheckPermission;
+import com.now.nowbot.model.match.Match;
 import com.now.nowbot.service.OsuGetService;
 import net.mamoe.mirai.contact.Group;
 import net.mamoe.mirai.event.events.MessageEvent;
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 
 @Service("t-ra")
@@ -46,48 +47,42 @@ public class TestRaService implements MessageService {
     }
 
     void mo(int id, long eventid, StringBuffer strData) {
-        JsonNode data =null;
-//        if (eventid > 0) {
-//            data = osuGetService.getMatchInfo(id, eventid);
-//        } else {
-//            data = osuGetService.getMatchInfo(id);
-//        }
-        var events = data.get("events");
-        //因为ppy对大于100条event采用的是分页查询,先递归至房间创建的页,然后顺页执行
-        if (!events.get(0).get("detail").get("type").asText().equals("match-created")) {
-            mo(id, events.get(0).get("id").asLong(), strData);
+        Match match =null;
+        while (!match.getFirstEventId().equals(match.getEvents().get(0).getId())) {
+            var events = osuGetService.getMatchInfo(id, match.getEvents().get(0).getId()).getEvents();
+            match.getEvents().addAll(0, events);
         }
 
-
+        var events = match.getEvents();
         for (var node : events) {
-            if (node.get("detail").get("type").asText("no").equals("other")) {
-                var game = node.get("game");
+            if (node.getGame() != null) {
+                var game = node.getGame();
                 try {
-                    strData.append(LocalDateTime.from(f1.parse(game.get("start_time").asText())).format(f2)).append(' ')
-                            .append(game.get("mode").asText()).append(' ')
-                            .append(game.get("scoring_type").asText()).append(' ')
-                            .append(game.get("team_type").asText()).append(' ')
-                            .append((game.get("beatmap").get("difficulty_rating").asDouble())).append(' ')
-                            .append(game.get("beatmap").get("total_length").asText()).append(' ')
-                            .append(game.get("mods").toString()).append(' ')
-                            .append(game.get("beatmap").get("id").asInt()).append(' ')
-                            .append(osuGetService.getMapInfo(game.get("beatmap").get("id").asInt()).getMaxCombo()).append(' ')
+                    strData.append(LocalDateTime.from(f1.parse(game.getScoringType())).format(f2)).append(' ')
+                            .append(game.getMode()).append(' ')
+                            .append(game.getScoringType()).append(' ')
+                            .append(game.getTeamType()).append(' ')
+                            .append((game.getBeatmap().getDifficultyRating())).append(' ')
+                            .append(game.getBeatmap().getTotalLength()).append(' ')
+                            .append(Arrays.toString(game.getMods())).append(' ')
+                            .append(game.getBeatmap().getId()).append(' ')
+                            .append(osuGetService.getMapInfo(game.getBeatmap().getId()).getMaxCombo()).append(' ')
                             .append('\n');
                 } catch (Exception e) {
                     strData.append(e.getMessage()).append('\n');//.append("  error---->")
                 }
-                for (var score : game.get("scores")) {
+                for (var score : game.getScoreInfos()) {
                     try {
-                        strData.append(score.get("user_id").asText()).append(' ')
-                                .append((score.get("accuracy").asText() + "     "), 0, 6).append(' ')
-                                .append(score.get("mods").toString()).append(' ')
-                                .append(score.get("score").asText()).append(' ')
-                                .append(score.get("max_combo").asText()).append(' ')
-                                .append(score.get("passed").asText()).append(' ')
-                                .append(score.get("perfect").asInt() != 0).append(' ')
-                                .append(score.get("match").get("slot").asText()).append(' ')
-                                .append(score.get("match").get("team").asText()).append(' ')
-                                .append(score.get("match").get("pass").asText()).append(' ')
+                        strData.append(score.getUserId()).append(' ')
+                                .append((score.getAccuracy() + "     "), 0, 6).append(' ')
+                                .append(Arrays.toString(score.getMods())).append(' ')
+                                .append(score.getScore()).append(' ')
+                                .append(score.getMaxCombo()).append(' ')
+                                .append(score.getPassed()).append(' ')
+                                .append(score.getPerfect() != 0).append(' ')
+                                .append(score.getMatch().get("slot").asText()).append(' ')
+                                .append(score.getMatch().get("team").asText()).append(' ')
+                                .append(score.getMatch().get("pass").asText()).append(' ')
                                 .append("\n");
                     } catch (Exception e) {
                         strData.append("  error---->").append(e.getMessage()).append('\n');
