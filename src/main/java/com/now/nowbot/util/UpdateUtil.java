8d7840848b8d00ec58b00ddb5a4ef48fd4c7966c
Your Name <365246692@qq.com>
2022-08-08 23:29:37 +0800

    UpdateUtil 路径

--- src/main/java/com/now/nowbot/util/UpdateUtil.java
+++ src/main/java/com/now/nowbot/util/UpdateUtil.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class UpdateUtil {
     static public final void update() throws IOException {
 
-        Runtime.getRuntime().exec("/root/update.sh");
+        Runtime.getRuntime().exec("/home/spring/update.sh");
 
         System.exit(0);
     }
