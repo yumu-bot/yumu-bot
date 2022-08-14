3874e3f7969b92830d2df5c97e2f651d0cac286e
Your Name <365246692@qq.com>
2022-08-14 18:01:43 +0800

    cover ignore

--- src/main/java/com/now/nowbot/model/JsonData/Cover.java
+++ src/main/java/com/now/nowbot/model/JsonData/Cover.java
@@ -1,8 +1,10 @@
 package com.now.nowbot.model.JsonData;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class Cover {
     @JsonProperty("custom_url")
