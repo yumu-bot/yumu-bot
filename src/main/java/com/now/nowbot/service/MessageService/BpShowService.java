2a1d898dbef76ec9810369cdd8a38f1d89970af0
Your Name <365246692@qq.com>
2022-10-14 17:58:05 +0800

    1

--- src/main/java/com/now/nowbot/service/MessageService/BpShowService.java
+++ src/main/java/com/now/nowbot/service/MessageService/BpShowService.java
@@ -11,6 +11,6 @@ public class BpShowService implements MessageService {
     @Override
     public void HandleMessage(MessageEvent event, Matcher matcher) throws Throwable {
         var m = Integer.parseInt(matcher.group("n"));
-        event.getSubject().sendMessage(new At(event.getSender().getId()).plus("来了"));
+//        event.getSubject().sendMessage(new At(event.getSender().getId()).plus("来了"));
     }
 }
