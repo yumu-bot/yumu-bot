293fe344135d9ad05e18f8842ca2715d5b8e226b
Your Name <365246692@qq.com>
2023-01-13 17:29:12 +0800

    清理无用代码1

--- src/main/java/com/now/nowbot/NowbotApplication.java
+++ src/main/java/com/now/nowbot/NowbotApplication.java
@@ -1,19 +1,17 @@
 package com.now.nowbot;
 
-import com.now.nowbot.config.FileConfig;
-import com.now.nowbot.config.OSUConfig;
-import com.now.nowbot.config.QQConfig;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableConfigurationProperties({QQConfig.class, FileConfig.class, OSUConfig.class})
+@ConfigurationPropertiesScan
 @EnableAspectJAutoProxy(exposeProxy = true, proxyTargetClass = true)
 @EnableScheduling
 @EnableRetry
