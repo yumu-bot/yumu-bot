88ff9ee74e5894669f0dbe25efd6a163b3b6726e
yumemuzi <1340691940@qq.com>
2023-02-22 01:22:34 +0800

    颜色第三版更新

--- src/main/java/com/now/nowbot/model/match/UserMatchData.java
+++ src/main/java/com/now/nowbot/model/match/UserMatchData.java
@@ -395,48 +395,48 @@ public class UserMatchData {
 
     public enum Rating{
         // 这一段是 YMRA v1.2 更新内容
-        BC("Big Carry", Color.makeRGB(255,255,0)), //大爹
+        BC("Big Carry", Color.makeRGB(255,241,0)), //大爹
         CA("Carry", Color.makeRGB(255,152,0)), //大哥
-        MF("Main Force", Color.makeRGB(76,175,80)), //主力
-        SP("Specialized",Color.makeRGB(174,234,0)), //专精
-        WF("Work Force", Color.makeRGB(63,81,181)), //打工
+        MF("Main Force", Color.makeRGB(34,172,56)), //主力
+        SP("Specialized",Color.makeRGB(179,212,101)), //专精
+        WF("Work Force", Color.makeRGB(0,104,183)), //打工
         GE("General",Color.makeRGB(189,189,189)), //普通
-        GU("Guest", Color.makeRGB(66,165,245)), //客串
-        SU("Support",Color.makeRGB(103,58,183)), //抗压
-        SG("Scapegoat", Color.makeRGB(236,64,122)), //背锅
-        NO("Noob", Color.makeRGB(255,193,7)), //小弟
+        GU("Guest", Color.makeRGB(0,160,233)), //客串
+        SU("Support",Color.makeRGB(96,25,134)), //抗压
+        SG("Scapegoat", Color.makeRGB(228,0,127)), //背锅
+        NO("Noob", Color.makeRGB(235,104,119)), //小弟
         FU("Futile", Color.makeRGB(211,47,47)), //炮灰
 
-        SMA("Strongest Marshal", Color.makeRGB(255,255,0)),//最强元帅
-        CMA("Competent Marshal", Color.makeRGB(255,255,0)),//称职元帅
-        IMA("Indomitable Marshal", Color.makeRGB(255,255,0)),//不屈元帅
+        SMA("Strongest Marshal", Color.makeRGB(255,241,0)),//最强元帅
+        CMA("Competent Marshal", Color.makeRGB(255,241,0)),//称职元帅
+        IMA("Indomitable Marshal", Color.makeRGB(255,241,0)),//不屈元帅
         EGE("Ever-Victorious General", Color.makeRGB(255,152,0)),//常胜将军
         AGE("Assiduous General", Color.makeRGB(255,152,0)),//勤奋将军
         SGE("Striven General", Color.makeRGB(255,152,0)),//尽力将军
-        EMF("Effective Main Force", Color.makeRGB(76,175,80)),//突破主力
-        RMF("Reliable Main Force", Color.makeRGB(76,175,80)),//可靠主力
-        SMF("Staunch Main Force", Color.makeRGB(76,175,80)),//坚守主力
-        EAS("Elite Assassin", Color.makeRGB(174,234,0)),//精锐刺客
-        NAS("Normal Assassin", Color.makeRGB(174,234,0)),//普通刺客
-        FAS("Fake Assassin", Color.makeRGB(174,234,0)),//冒牌刺客
-        GCW("Gold Collar Worker", Color.makeRGB(63,81,181)),//金领工人
-        WCW("White Collar Worker", Color.makeRGB(63,81,181)),//白领工人
-        BCW("Blue Collar Worker", Color.makeRGB(63,81,181)),//蓝领工人
+        EMF("Effective Main Force", Color.makeRGB(34,172,56)),//突破主力
+        RMF("Reliable Main Force", Color.makeRGB(34,172,56)),//可靠主力
+        SMF("Staunch Main Force", Color.makeRGB(34,172,56)),//坚守主力
+        EAS("Elite Assassin", Color.makeRGB(179,212,101)),//精锐刺客
+        NAS("Normal Assassin", Color.makeRGB(179,212,101)),//普通刺客
+        FAS("Fake Assassin", Color.makeRGB(179,212,101)),//冒牌刺客
+        GCW("Gold Collar Worker", Color.makeRGB(0,104,183)),//金领工人
+        WCW("White Collar Worker", Color.makeRGB(0,104,183)),//白领工人
+        BCW("Blue Collar Worker", Color.makeRGB(0,104,183)),//蓝领工人
         KPS("Key Person", Color.makeRGB(189,189,189)),//关键人
         CMN("Common Man", Color.makeRGB(189,189,189)),//普通人
         PSB("Passer-by", Color.makeRGB(189,189,189)),//路人甲
-        MAC("Major Character", Color.makeRGB(66,165,245)),//主要角色
-        MIC("Minor Character", Color.makeRGB(66,165,245)),//次要角色
-        FIG("Figurant", Color.makeRGB(66,165,245)),//群众演员
-        SAM("Stable as Mountain", Color.makeRGB(103,58,183)),//稳如泰山
-        HAS("Hard as Stone", Color.makeRGB(103,58,183)),//坚若磐石
-        SIN("Seriously Injured", Color.makeRGB(103,58,183)),//伤痕累累
-        ANI("Advanced Ninja", Color.makeRGB(236,64,122)),//上等忍者
-        MNI("Mediocre Ninja", Color.makeRGB(236,64,122)),//普通忍者
-        LCS("Lower-class", Color.makeRGB(236,64,122)),//不入流
-        LKD("Lucky Dog", Color.makeRGB(255,193,7)),//幸运儿
-        QAP("Qualified Apprentice", Color.makeRGB(255,193,7)),//合格学徒
-        BGN("Beginner", Color.makeRGB(255,193,7)),//初学者
+        MAC("Major Character", Color.makeRGB(0,160,233)),//主要角色
+        MIC("Minor Character", Color.makeRGB(0,160,233)),//次要角色
+        FIG("Figurant", Color.makeRGB(0,160,233)),//群众演员
+        SAM("Stable as Mountain", Color.makeRGB(96,25,134)),//稳如泰山
+        HAS("Hard as Stone", Color.makeRGB(96,25,134)),//坚若磐石
+        SIN("Seriously Injured", Color.makeRGB(96,25,134)),//伤痕累累
+        ANI("Advanced Ninja", Color.makeRGB(228,0,127)),//上等忍者
+        MNI("Mediocre Ninja", Color.makeRGB(228,0,127)),//普通忍者
+        LCS("Lower-class", Color.makeRGB(228,0,127)),//不入流
+        LKD("Lucky Dog", Color.makeRGB(235,104,119)),//幸运儿
+        QAP("Qualified Apprentice", Color.makeRGB(235,104,119)),//合格学徒
+        BGN("Beginner", Color.makeRGB(235,104,119)),//初学者
         LSS("Life-saving Straw", Color.makeRGB(211,47,47)),//救命稻草
         LSP("Little Spark", Color.makeRGB(211,47,47)),//点点星火
         BDT("Burnt Dust", Color.makeRGB(211,47,47)),//湮灭尘埃
