dd0afbdb2233fc22b5ebc45e856eafd7f0832da1
YuriMumu <1340691940@qq.com>
2023-04-10 21:33:44 +0800

    帮助优化

--- src/main/java/com/now/nowbot/service/MessageService/HelpService.java
+++ src/main/java/com/now/nowbot/service/MessageService/HelpService.java
@@ -20,6 +20,24 @@ public class HelpService implements MessageService {
 //                sb.append(ins.getDesc()).append("\n");
 //        }
 //        from.sendMessage(sb.toString());
-        QQMsgUtil.sendImage(from,Files.readAllBytes(Path.of(NowbotConfig.BG_PATH).resolve("ExportFileV3/panel-help.png")));
+//        QQMsgUtil.sendImage(from,Files.readAllBytes(Path.of(NowbotConfig.BG_PATH).resolve("ExportFileV3/panel-help.png")));
+
+        String module = ""; //传东西进来
+        String path;
+        switch (module) {
+            case "bot", "b" : path = "help-bot.png"; break;
+            case "score", "s" : path = "help-score.png"; break;
+            case "player", "p" : path = "help-player.png"; break;
+            case "map", "m" : path = "help-map.png"; break;
+            case "custom", "c" : path = "help-custom.png"; break;
+            case "fun", "f" : path = "help-fun.png"; break;
+            case "aid", "a" : path = "help-aid.png"; break;
+            case "tournament", "t" : path = "help-tournament.png"; break;
+            case default : path = "help-default.png"; break;
+        }
+
+        path = "ExportFileV3/" + path;
+
+        QQMsgUtil.sendImage(from,Files.readAllBytes(Path.of(NowbotConfig.BG_PATH).resolve(path)));
     }
 }
