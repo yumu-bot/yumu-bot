1c507bfb185195d283d917adea6b8f7731c46284
Your Name <365246692@qq.com>
2023-05-26 17:30:21 +0800

    fix 修正语法

--- src/main/java/com/now/nowbot/mapper/DrawLogLiteRepository.java
+++ src/main/java/com/now/nowbot/mapper/DrawLogLiteRepository.java
@@ -22,7 +22,7 @@ public interface DrawLogLiteRepository extends JpaRepository<DrawLogLite, Long> 
      * @param kinds 出的品级
      * @return 多少抽之内出的选定品级结果总和
      */
-    @Query(value = "select count(*) from (select * from test_draw where uid=:uid order by create_at desc limit :n) where kind in :#{#kinds.![ordinal()]}", nativeQuery = true)
+    @Query(value = "select count(*) from (select * from test_draw where uid=:uid order by id desc limit :n) as dlist where dlist.kind in :#{#kinds.![ordinal()]}", nativeQuery = true)
     Integer getKindCount(long uid, int n, @Param("kinds") DrawKind... kinds);
 
     /***
@@ -31,7 +31,7 @@ public interface DrawLogLiteRepository extends JpaRepository<DrawLogLite, Long> 
      * @param kinds 品级
      * @return id
      */
-    @Query(value = "select id from test_draw where uid=:uid and kind in :#{#kinds.![ordinal()]} order by create_at desc limit 1", nativeQuery = true)
+    @Query(value = "select id from test_draw where uid=:uid and kind in :#{#kinds.![ordinal()]} order by id desc limit 1", nativeQuery = true)
     Optional<Long> getBeforId(long uid, @Param("kinds") DrawKind... kinds);
 
     /***
