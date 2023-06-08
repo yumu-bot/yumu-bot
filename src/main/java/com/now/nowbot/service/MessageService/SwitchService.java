192f4a536451cc3ede7f7f9c47f11c85b59aece3
YuriMumu <1340691940@qq.com>
2023-06-09 00:12:35 +0800

    更合理的SW

--- src/main/java/com/now/nowbot/service/MessageService/SwitchService.java
+++ src/main/java/com/now/nowbot/service/MessageService/SwitchService.java
@@ -34,49 +34,41 @@ public class SwitchService implements MessageService{
         }
         if (p1 == null) {
             from.sendMessage("""
-                    wake/sleep <time>
-                    list->out all name; open/close <name>
-                    banlist->out all changeable name; ban/unban <name or "ALL"> <qq/group>
+                    [sleep] wake/sleep <time>
+                    [list] export all available service: <servicename> on/off
+                    [banlist] export all operational name: ban/unban <name/"ALL"> <qq/group>
+                    
                     """);
+            // 等同于 case list
+
+            StringBuilder sb = new StringBuilder();
+            var list = Permission.getClouseServices();
+            for (Instruction value : Instruction.values()) {
+                sb.append(value).append(list.contains(value)?'OFF':'ON').append('\n');
+            }
+            from.sendMessage(sb.toString());
             return;
         }
-        switch (p1.toLowerCase()){
+
+        // sleep awake 功能应该放在其他的地方吧，比如新开一个重启bot的服务，里面附带这个功能
+        // 不对吧，这是禁言别人然后让别人休眠的东西？
+        switch (p1.toLowerCase()) {
             case "sleep" -> {
                 if (p2 != null){
                     try {
                         int time = Integer.parseInt(p2);
+                        from.sendMessage("晚安！");
                         SendmsgUtil.addSleep(event.getSubject().getId(), time);
-                    }catch (NumberFormatException e){
-                        from.sendMessage("参数异常");
+                    } catch (NumberFormatException e){
+                        from.sendMessage("请输入正确的休眠参数！");
                     }
                 }
             }
             case "wake" -> {
                 SendmsgUtil.wakeUp(event.getSubject().getId());
-                from.sendMessage("恢复");
+                from.sendMessage("早安！");
             }
-            case "close" -> {
-                if (p2 != null){
-                    try {
-                        var i = Instruction.valueOf(p2.toUpperCase());
-                        Permission.clouseService(i);
-                        from.sendMessage("已关闭");
-                    } catch (IllegalArgumentException e) {
-                        from.sendMessage("没找到这个服务");
-                    }
-                }
-            }
-            case "open" -> {
-                if (p2 != null){
-                    try {
-                        var i = Instruction.valueOf(p2.toUpperCase());
-                        Permission.openService(i);
-                        from.sendMessage("已启动");
-                    } catch (IllegalArgumentException e) {
-                        from.sendMessage("没找到这个服务");
-                    }
-                }
-            }
+
             case "list" -> {
                 StringBuilder sb = new StringBuilder();
                 var list = Permission.getClouseServices();
@@ -84,7 +76,9 @@ public class SwitchService implements MessageService{
                     sb.append(value).append(list.contains(value)?'×':'√').append('\n');
                 }
                 from.sendMessage(sb.toString());
+                return;
             }
+
             case "banlist" -> {
                 var texts = permission.list();
                 var random = new Random();
@@ -93,8 +87,31 @@ public class SwitchService implements MessageService{
                     event.getSubject().sendMessage(text);
                 }
             }
+        }
 
+        switch (p2.toLowerCase()){
+            case "off" -> {
+                if (p1 != null){
+                    try {
+                        var i = Instruction.valueOf(p1.toUpperCase());
+                        Permission.clouseService(i);
+                        from.sendMessage("已关闭服务");
+                    } catch (IllegalArgumentException e) {
+                        from.sendMessage("没找到这个服务");
+                    }
+                }
+            }
+            case "on" -> {
+                if (p1 != null){
+                    try {
+                        var i = Instruction.valueOf(p1.toUpperCase());
+                        Permission.openService(i);
+                        from.sendMessage("已启动服务");
+                    } catch (IllegalArgumentException e) {
+                        from.sendMessage("没找到这个服务");
+                    }
+                }
+            }
         }
     }
-
 }
