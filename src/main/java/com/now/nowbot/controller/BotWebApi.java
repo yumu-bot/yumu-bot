c472e2cd0dfd1fb579cbaf8168b26aaae12040a1
YuriMumu <1340691940@qq.com>
2023-08-15 00:20:42 +0800

    临时添加跨域

--- src/main/java/com/now/nowbot/controller/BotWebApi.java
+++ src/main/java/com/now/nowbot/controller/BotWebApi.java
@@ -37,6 +37,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/pub", method = RequestMethod.GET)
+@CrossOrigin
 public class BotWebApi {
     private static final Logger log = LoggerFactory.getLogger(BotWebApi.class);
     @Resource
