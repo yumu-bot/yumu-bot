e3c347b18e101318799db6cce7d49e2d51938f1c
spring <365246692@qq.com>
2023-10-09 16:54:35 +0800

    delete legacy

--- src/main/java/com/now/nowbot/util/Instruction.java
+++ src/main/java/com/now/nowbot/util/Instruction.java
@@ -9,17 +9,6 @@ import java.util.regex.Pattern;
 /**
  * 所有的指令都写在这里方便进行管理
  **/
-// * 正则
-// * (?:pattern) 匹配 pattern 但不捕获该匹配的子表达式,即它是一个非捕获匹配,不存储供以后使用的匹配.
-// * 例子: "industr(?:y|ies)" 是比 "industry|industries" 更经济的表达式.
-// * (?!pattern) 执行反向预测先行搜索的子表达式,该表达式匹配不处于匹配 pattern 的字符串的起始点的搜索字符串,不占用字符,即发生匹配后,下一匹配的搜索紧随上一匹配之后.
-// * (?<!pattern)执行正向预测先行搜索的子表达式,该表达式与上条相反,同时也不占用字符 (其实 (?!) 跟 (?<!) 作用相同,区别是下次匹配的指针位置)
-// * 例子: "(?<!ab)cd(?!ef)" 仅匹配非ab开头的,ef结尾的cd,且ab与ef仅作搜索用并不占用宽度,即例子对gcd中的cd也会被匹配
-// * (?=pattern) 也叫零宽度正预测先行断言,它断言被匹配的字符串以表达式pattern结尾但除了结尾以外的部分,预测先行不占用字符,即发生匹配后,下一匹配的搜索紧随上一匹配之后.
-// * (?<=pattern)也叫零宽度正回顾后发断言，它断言自身出现的位置的前面能匹配表达式pattern,回顾后发断言也不占用字符.
-// * 例子: "(?<=\d)(匹配位置)(?=(\d{4})+$)" 仅匹配开头为数字,且长度为4n的纯数字结尾,零宽度断言并不占用字符(即不包含自身),当匹配位置为空时满足匹配且宽度为0.
-// * (#正则注释) 仅作注释,会被匹配器忽略掉的文字
-// * p.s. 以上的不占用字符,可理解为允许这样的匹配格式,但是已匹配的内容可能之后被重复匹配,且无法被Matcher.group()获取到,通常情况下适用于 替换文本/匹配固定'指令及参数'想获得参数值但是不想获得指令本身
 
 public enum Instruction {
 
@@ -57,18 +46,8 @@ public enum Instruction {
     // b ymb ymbp :0-3 name 1-100
     BP(BPService.class,
             null),
-
-    /*
-        BP(BPService.class,
-                Pattern.compile("^[!！]\\s*(?i)(ym)?(bestperformance|bp(?!\\w)|b(?!\\w))+\\s*([:：](?<mode>\\w+))?\\s*(?<name>[0-9a-zA-Z\\[\\]\\-_ ]*?(?!\\d+))?\\s*((?<n>\\d+)(-(?<m>\\d+))?)$")),
-
-     */
-    BPLEGACY(BPLegacyService.class,
-            Pattern.compile("^[!！]\\s*(?i)(ym)?(bestperformancelegacy|bpl(?![a-zA-Z_])|bl(?![a-zA-Z_]))+\\s*([:：](?<mode>\\w+))?\\s*(?<name>[0-9a-zA-Z\\[\\]\\-_ ]*?)?\\s*(#?(?<n>\\d+)(-(?<m>\\d+))?)?$")),
     TODAYBP(TodayBPService.class,
             Pattern.compile("^[!！]\\s*(?i)(ym)?(todaybp|(tbp|tdp|t(?![a-zA-Z_])))+\\s*([:：](?<mode>[\\w\\d]+))?\\s*(?<name>[0-9a-zA-Z\\[\\]\\-_ ]*?(?!#))?\\s*(#?\\s*(?<day>\\d*)\\s*)$")),
-    TODAYBPLEGACY(TBPLegacyService.class,
-            Pattern.compile("^[!！]\\s*(?i)(ym)?(todaybplegacy|tbpl(?![a-zA-Z_])|tl(?![a-zA-Z_]))+\\s*([:：](?<mode>[\\w\\d]+))?\\s*(?<name>[0-9a-zA-Z\\[\\]\\-_ ]*?(?!#))?\\s*(#?\\s*(?<day>\\d*)\\s*)$")),
 
     BPA(BPAnalysisService.class,
             Pattern.compile("^[!！]\\s*(?i)(ym)?((bpanalysis)|(blue\\s*archive)|bpa(?![a-zA-Z_])|ba(?![a-zA-Z_]))+(\\s*[:：](?<mode>\\w+))?(\\s+(?<name>[0-9a-zA-Z\\[\\]\\-_ ]*))?")),
@@ -87,8 +66,6 @@ public enum Instruction {
     IM(IMapperService.class,
             Pattern.compile("^[!！]\\s*(?i)(ym)?((im?)?mapper|im(?![a-zA-Z_]))+(\\s*(?<name>[0-9a-zA-Z\\[\\]\\-_ ]*))?")),
 
-    FRIENDLEGACY(FriendLegacyService.class,
-            Pattern.compile("^[!！]\\s*(?i)(ym)?(friendlegacy|fl(?![a-zA-Z_]))+(\\s*(?<n>\\d+))?(\\s*[:-]\\s*(?<m>\\d+))?")),
 
     FRIEND(FriendService.class,
             Pattern.compile("^[!！]\\s*(?i)(ym)?(friend(s)?|f(?![a-zA-Z_]))+\\s*(?<n>\\d+)?(\\s*[:-]\\s*(?<m>\\d+))?")),
@@ -98,9 +75,6 @@ public enum Instruction {
 
     PPM(PPMinusService.class,
             Pattern.compile("^[!！]\\s*(?i)(ym)?(ppminus|(p?(pm))(?![a-rt-uw-zA-RT-UW-Z_]))\\s*(?<vs>vs)?\\s*([:：](?<mode>[\\w\\d]+))?(\\s*(?<name>[0-9a-zA-Z\\[\\]\\-_ ]*))?")),
-
-    PPMLEGACY(PPMLegacyService.class,
-            Pattern.compile("^[!！]\\s*(?i)(ym)?(pl(?![a-zA-Z_])|ppminuslegacy)+(?<vs>vs)?([:：](?<mode>[\\w\\d]+))?(\\s+(?<name>[0-9a-zA-Z\\[\\]\\-_ ]*))?")),
 
 
     // #4 osu! 谱面指令
@@ -119,8 +93,6 @@ public enum Instruction {
 
     // #5 osu! 比赛指令
 
-    MRALEGACY(MRALegacyService.class,
-            Pattern.compile("^[!！]\\s*(?i)(ym)?(ratinglegacy|rav2(?![a-zA-Z_])|ral(?![a-zA-Z_])|rl(?![a-zA-Z_]))+\\s*(?<matchid>\\d+)(\\s+(?<skipedrounds>\\d+))?(\\s+(?<deletendrounds>\\d+))?(\\s+(?<includingfail>\\d))?")),
 
     MRA(MRAService.class,
             Pattern.compile("^[!！]\\s*(?i)((ym)?rating|(ym)?ra(?![a-zA-Z_])|mra(?![a-zA-Z_]))+\\s*(?<matchid>\\d+)(\\s*(?<skipedrounds>\\d+))?(\\s*(?<deletendrounds>\\d+))?(\\s*(?<excludingrematch>[Rr]))?(\\s*(?<excludingfail>[Ff]))?")),
@@ -178,8 +150,6 @@ public enum Instruction {
     TESTPPM(TestPPMService.class,
             Pattern.compile("[!！]\\s*(?i)testppm(\\s*[:：](?<mode>[\\w\\d]+))?(\\s+(?<name>[0-9a-zA-Z\\[\\]\\-_ ]*))")),
 
-    TESTINFO(InfoLegacyService.class,
-            Pattern.compile("[!！]\\s*(?i)(testinfo)([:：](?<mode>[\\w\\d]+))?(\\s+(?<name>[0-9a-zA-Z\\[\\]\\-_ ]*))")),
 
     TESTTAIKOSTARCALCULATE(TestMt4.class,
             Pattern.compile("^[!！]\\s*(?i)testmt\\s*(?<data>[ox ]+)")),
@@ -221,20 +191,5 @@ public enum Instruction {
 
     public Class<? extends MessageService> getaClass() {
         return aClass;
-    }
-
-    public static void main(String[] args) {
-        var p = Instruction.BIND.regex;
-        var m = p.matcher("!ymbind");
-        if (m.matches()) {
-            System.out.println("ok************");
-
-            System.out.println(m.group("un"));
-        }
-//        var rp = valueOf("ban".toUpperCase());
-//        var rm = rp.regex.matcher("!ban ymf f 22");
-//        System.out.println(rm.find());
-//        System.out.println(rm.group("gf"));
-
     }
 }
