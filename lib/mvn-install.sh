921cd03fb2f4c07eb26ce39e6521926d6492ea7a
Your Name <365246692@qq.com>
2023-10-14 15:08:53 +0800

    rename

--- lib/mvn-install.sh
+++ lib/mvn-install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mvn install:install-file -Dfile="shiro-relese.jar" -DgroupId="com.mikuac" -DartifactId="shiro" -Dversion="relese" -Dpackaging=jar
+mvn install:install-file -Dfile="shiro-release.jar" -DgroupId="com.mikuac" -DartifactId="shiro" -Dversion="release" -Dpackaging=jar
 mvn install:install-file -Dfile="YurnSatoriFramework-0.0.2.jar" -DgroupId="com.yurn" -DartifactId="YurnSatoriFramework" -Dversion="0.0.2" -Dpackaging=jar
