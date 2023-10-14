cf5698eac2b595ffbf2f57cac1bf82918e828181
Your Name <365246692@qq.com>
2023-10-14 17:32:02 +0800

    ok

--- src/main/java/com/now/nowbot/listener/OneBotListener.java
+++ src/main/java/com/now/nowbot/listener/OneBotListener.java
@@ -37,7 +37,6 @@ public class OneBotListener {
     @GroupMessageHandler()
     @Async
     public void handle(Bot bot, GroupMessageEvent onebotEvent) {
-        if (bot != null) return;
         var event = new com.now.nowbot.qq.onebot.event.GroupMessageEvent(bot, onebotEvent);
         log.trace("收到消息[{}] -> {}", event.getSubject().getId(), ShiroUtils.unescape(onebotEvent.getRawMessage()));
         ASyncMessageUtil.put(event);
