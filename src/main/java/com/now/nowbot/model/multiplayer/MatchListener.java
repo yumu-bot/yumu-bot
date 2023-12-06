e1c4cf1e504f3021ef5afe5aa98a0bbfebdbe330
spring <365246692@qq.com>
2023-12-06 14:14:37 +0800

    update

--- src/main/java/com/now/nowbot/model/multiplayer/MatchListener.java
+++ src/main/java/com/now/nowbot/model/multiplayer/MatchListener.java
@@ -1,10 +1,13 @@
 package com.now.nowbot.model.multiplayer;
 
 import com.now.nowbot.service.OsuApiService.OsuMatchApiService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -13,6 +16,7 @@ import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class MatchListener {
+    private static final Logger log = LoggerFactory.getLogger(MatchListener.class);
     private static final ScheduledExecutorService executorService;
 
     static {
@@ -68,9 +72,11 @@ public class MatchListener {
         recordID = match.getLatestEventId();
 
         if (Objects.nonNull(match.getCurrentGameId())) {
-            -- recordID;
+            Optional<MatchEvent> gameOpt = getLastRound(match.getEvents());
+            var game = gameOpt.orElseThrow();
+            recordID = game.getId() - 1;
             // 刚开始时如果有正在进行的对局, 额外提前触发一次开始事件
-            var nowPlaying = List.of(match.getEvents().getLast());
+            var nowPlaying = List.of(game);
             onEvents(nowPlaying, match);
         }
 
@@ -83,26 +89,43 @@ public class MatchListener {
         return Objects.nonNull(future) && ! future.isDone();
     }
 
-    private void listen() {
-        if (match.isMatchEnd()) {
-            this.stopListener();
-        }
-        var newMatch = matchApiService.getMatchInfoAfter(matchID, recordID);
-
-        if (newMatch.getLatestEventId() == recordID) return;
-
-        if (Objects.nonNull(newMatch.getCurrentGameId())) {
-            if (newMatch.getLatestEventId() - 1 != recordID) {
-                recordID = newMatch.getLatestEventId() - 1;
-                onEvents(newMatch.getEvents(), match);
+    private Optional<MatchEvent> getLastRound(List<MatchEvent> events) {
+        MatchEvent e = null;
+        var iter = events.listIterator(events.size());
+        while (iter.hasPrevious()) {
+            var event = iter.previous();
+            if (Objects.nonNull(event.getRound())) {
+                e = event;
+                break;
             }
-            return;
-        } else {
-            recordID = newMatch.getLatestEventId();
         }
+        return Optional.ofNullable(e);
+    }
 
-        match.parseNextData(newMatch);
-        onEvents(newMatch.getEvents(), match);
+    private void listen() {
+        try {
+            if (match.isMatchEnd()) {
+                this.stopListener();
+            }
+            var newMatch = matchApiService.getMatchInfoAfter(matchID, recordID);
+
+            if (newMatch.getLatestEventId() == recordID) return;
+
+            if (Objects.nonNull(newMatch.getCurrentGameId())) {
+                if (newMatch.getLatestEventId() - 1 != recordID) {
+                    recordID = newMatch.getLatestEventId() - 1;
+                    onEvents(newMatch.getEvents(), match);
+                }
+                return;
+            } else {
+                recordID = newMatch.getLatestEventId();
+            }
+
+            match.parseNextData(newMatch);
+            onEvents(newMatch.getEvents(), match);
+        } catch (Exception e) {
+            log.error("监听比赛期间出现错误: ", e);
+        }
     }
 
     public void stopListener() {
