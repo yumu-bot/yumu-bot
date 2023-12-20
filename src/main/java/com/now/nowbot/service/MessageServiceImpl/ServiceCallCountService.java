90ca33d974d29f45148c067b7505bd14d8f2d4d2
spring <365246692@qq.com>
2023-12-20 14:33:47 +0800

    update

--- src/main/java/com/now/nowbot/service/MessageServiceImpl/ServiceCallCountService.java
+++ src/main/java/com/now/nowbot/service/MessageServiceImpl/ServiceCallCountService.java
@@ -9,12 +9,17 @@ import com.now.nowbot.service.MessageService;
 import com.now.nowbot.util.QQMsgUtil;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Objects;
 import java.util.function.Consumer;
 
 @Service("SERVICE_CALL_COUNT")
-public class ServiceCallCountService implements MessageService<Object> {
+public class ServiceCallCountService implements MessageService<Integer> {
     private final ServiceCallRepository serviceCallRepository;
     private final ImageService          imageService;
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM-dd HH");
 
     public ServiceCallCountService(ServiceCallRepository serviceCallRepository,
                                    ImageService imageService) {
@@ -23,18 +28,34 @@ public class ServiceCallCountService implements MessageService<Object> {
     }
 
     @Override
-    public boolean isHandle(MessageEvent event, DataValue<Object> data) throws Throwable {
-        return event.getRawMessage().equals("统计服务调用");
+    public boolean isHandle(MessageEvent event, DataValue<Integer> data) throws Throwable {
+        if (! event.getRawMessage().startsWith("统计服务调用")) return false;
+        var s = event.getRawMessage().split("\\s+");
+        if (s.length == 2) {
+            try {
+                data.setValue(Integer.parseInt(s[1]));
+            } catch (NumberFormatException ignore) {
+            }
+        }
+        return true;
     }
 
     @Override
     @CheckPermission(isSuperAdmin = true)
-    public void HandleMessage(MessageEvent event, Object matcher) throws Throwable {
+    public void HandleMessage(MessageEvent event, Integer hours) throws Throwable {
         StringBuilder sb = new StringBuilder("""
                 | 服务名 | 调用次数 | 平均用时 | 最大用时 | 最小用时 |
                 |-------|--------|---------|---------|---------|
                 """);
-        var result = serviceCallRepository.countAll();
+        List<ServiceCallLite.ServiceCallResult> result;
+        if (Objects.isNull(hours)) {
+            result = serviceCallRepository.countAll();
+        } else {
+            var now = LocalDateTime.now();
+            var bef = now.minusHours(hours);
+            event.getSubject().sendMessage(STR."处理 \{bef.format(dateTimeFormatter)} - \{now.format(dateTimeFormatter)}}");
+            result = serviceCallRepository.countBetwen(bef, now);
+        }
         Consumer<ServiceCallLite.ServiceCallResult> work = r -> sb
                 .append('|').append(r.getService())
                 .append('|').append(r.getSize())
