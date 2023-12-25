0075bff209b380b14d698ac2f30919613298d0aa
spring <365246692@qq.com>
2023-12-25 09:49:00 +0800

    add user history data

--- src/main/java/com/now/nowbot/dao/OsuUserInfoDao.java
+++ src/main/java/com/now/nowbot/dao/OsuUserInfoDao.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
@@ -49,12 +50,51 @@ public class OsuUserInfoDao {
         osuUserInfoMapper.saveAllAndFlush(all);
     }
 
-    public Optional<OsuUserInfoArchiveLite> getLast(Long uid, LocalDate date) {
-        return osuUserInfoMapper.selectDayLast(uid, date);
+    public static OsuUser fromArchive(OsuUserInfoArchiveLite archive) {
+        OsuUser user = new OsuUser();
+        user.setPlayMode(archive.getMode().getName());
+        user.setUID(archive.getOsuID());
+
+        Statistics statistics = new Statistics();
+        statistics.setA(statistics.getA());
+        statistics.setS(statistics.getS());
+        statistics.setSS(statistics.getSS());
+        statistics.setSH(statistics.getSH());
+        statistics.setSSH(statistics.getSSH());
+
+        statistics.setTotalScore(statistics.getTotalScore());
+        statistics.setRankedScore(statistics.getRankedScore());
+        statistics.setAccuracy(archive.getHit_accuracy());
+        statistics.setPlayCount(archive.getPlay_count());
+        statistics.setPlayTime(archive.getPlay_time());
+        statistics.setLevelCurrent(archive.getLevel_current());
+        statistics.setLevelProgress(archive.getLevel_progress());
+        statistics.setMaxCombo(archive.getMaximum_combo());
+        statistics.setPp(archive.getPP());
+
+        user.setStatistics(statistics);
+        return user;
     }
 
-    public Optional<OsuUserInfoArchiveLite> getLast(Long uid) {
-        return osuUserInfoMapper.selectLast(uid);
+    /**
+     * 取那一天最后的数据
+     *
+     * @param date 当天
+     * @return
+     */
+    public Optional<OsuUserInfoArchiveLite> getLast(Long uid, OsuMode mode, LocalDate date) {
+        return osuUserInfoMapper.selectDayLast(uid, mode, date);
+    }
+
+    /**
+     * 取 到那一天为止 最后的数据 (默认向前取一年)
+     *
+     * @param date 那一天
+     * @return
+     */
+    public Optional<OsuUserInfoArchiveLite> getLastFrom(Long uid, OsuMode mode, LocalDate date) {
+        var time = LocalDateTime.of(date, LocalTime.MAX);
+        return osuUserInfoMapper.selectDayLast(uid, mode, time.minusYears(1), time);
     }
 
     public static OsuUserInfoArchiveLite fromModel(OsuUser data) {
@@ -107,5 +147,9 @@ public class OsuUserInfoDao {
         out.setMaximum_combo(s.getMaxCombo());
 
         return out;
+    }
+
+    public Optional<OsuUserInfoArchiveLite> getLast(Long uid, OsuMode mode) {
+        return osuUserInfoMapper.selectLast(uid, mode);
     }
 }
--- src/main/java/com/now/nowbot/mapper/OsuUserInfoMapper.java
+++ src/main/java/com/now/nowbot/mapper/OsuUserInfoMapper.java
@@ -1,6 +1,7 @@
 package com.now.nowbot.mapper;
 
 import com.now.nowbot.entity.OsuUserInfoArchiveLite;
+import com.now.nowbot.model.enums.OsuMode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -13,14 +14,14 @@ import java.util.Optional;
 public interface OsuUserInfoMapper extends JpaRepository<OsuUserInfoArchiveLite, Long>, JpaSpecificationExecutor<OsuUserInfoArchiveLite> {
     OsuUserInfoArchiveLite getFirstByOsuIDOrderByTimeDesc(long osuid);
 
-    @Query("select o from OsuUserInfoArchiveLite o where o.osuID = :osuId and (o.time between :time1 and :time2) order by o.time desc ")
-    Optional<OsuUserInfoArchiveLite> selectDayLast(Long osuId, LocalDateTime time1, LocalDateTime time2);
+default Optional<OsuUserInfoArchiveLite> selectDayLast(Long osuId, OsuMode mode, LocalDate date) {
+        return selectDayLast(osuId, mode, LocalDateTime.of(date, LocalTime.MIN), LocalDateTime.of(date, LocalTime.MAX));
+    }
 
-    default Optional<OsuUserInfoArchiveLite> selectDayLast(Long osuId, LocalDate date) {
-        return selectDayLast(osuId, LocalDateTime.of(date, LocalTime.MIDNIGHT), LocalDateTime.of(date, LocalTime.MAX));
-    };
+        @Query("select o from OsuUserInfoArchiveLite o where o.osuID = :osuId and o.mode = :mode and (o.time between :time1 and :time2) order by o.time desc limit 1")
+    Optional<OsuUserInfoArchiveLite> selectDayLast(Long osuId, OsuMode mode, LocalDateTime time1, LocalDateTime time2);;
 
-    @Query("select o from OsuUserInfoArchiveLite o where o.osuID = :osuId order by o.time desc ")
-    Optional<OsuUserInfoArchiveLite> selectLast(Long osuId);
+    @Query("select o from OsuUserInfoArchiveLite o where o.osuID = :osuId and o.mode = :mode order by o.time desc limit 1")
+    Optional<OsuUserInfoArchiveLite> selectLast(Long osuId, OsuMode mode);
 
 }
