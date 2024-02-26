2f2417e197c773c7199dcec098b2cf127c16629f
spring <365246692@qq.com>
2024-02-26 10:45:53 +0800

    update sc

--- src/main/java/com/now/nowbot/service/MessageServiceImpl/ServiceCountService.java
+++ src/main/java/com/now/nowbot/service/MessageServiceImpl/ServiceCountService.java
@@ -68,11 +68,15 @@ public class ServiceCountService implements MessageService<Integer> {
         var from = event.getSubject();
         StringBuilder sb = new StringBuilder();
         List<ServiceCallLite.ServiceCallResult> result;
-        List<ServiceCallLite.ServiceCallResult$80> r80;
+        List<ServiceCallLite.ServiceCallResultLimit> r1;
+        List<ServiceCallLite.ServiceCallResultLimit> r80;
+        List<ServiceCallLite.ServiceCallResultLimit> r99;
         if (Objects.isNull(hours) || hours == 0) {
             result = serviceCallRepository.countAll();
             var now = LocalDateTime.now();
-            r80 = serviceCallRepository.countBetween$80(now.minusHours(24), now);
+            r1 = serviceCallRepository.countBetweenLimit(now.minusHours(24), now, 0.01);
+            r80 = serviceCallRepository.countBetweenLimit(now.minusHours(24), now, 0.80);
+            r99 = serviceCallRepository.countBetweenLimit(now.minusHours(24), now, 0.99);
             sb.append("## 时间段：迄今为止\n");
         } else {
             var now = LocalDateTime.now();
@@ -80,24 +84,36 @@ public class ServiceCountService implements MessageService<Integer> {
             //event.getSubject().sendMessage(STR."处理 [\{before.format(dateTimeFormatter)}] - [\{now.format(dateTimeFormatter)}]");
             sb.append(STR."## 时间段：**\{before.format(dateTimeFormatter)}** - **\{now.format(dateTimeFormatter)}**\n");
             result = serviceCallRepository.countBetween(before, now);
-            r80 = serviceCallRepository.countBetween$80(before, now);
+            r1 = serviceCallRepository.countBetweenLimit(before, now, 0.01);
+            r80 = serviceCallRepository.countBetweenLimit(before, now, 0.80);
+            r99 = serviceCallRepository.countBetweenLimit(before, now, 0.99);
         }
+        Map<String, Long> r1map = r1.stream().collect(Collectors.toMap(
+                ServiceCallLite.ServiceCallResultLimit::getService,
+                ServiceCallLite.ServiceCallResultLimit::getData
+        ));
         Map<String, Long> r80map = r80.stream().collect(Collectors.toMap(
-                ServiceCallLite.ServiceCallResult$80::getService,
-                ServiceCallLite.ServiceCallResult$80::getData
+                ServiceCallLite.ServiceCallResultLimit::getService,
+                ServiceCallLite.ServiceCallResultLimit::getData
+        ));
+        Map<String, Long> r99map = r99.stream().collect(Collectors.toMap(
+                ServiceCallLite.ServiceCallResultLimit::getService,
+                ServiceCallLite.ServiceCallResultLimit::getData
         ));
 
         sb.append("""
-                | 服务名 | 调用次数 | 最大用时 | 80% 用时 | 平均用时 | 最小用时 |
+                | 服务名 | 调用次数 | 最大用时 | 平均用时 | 最小用时 | 1% 用时 | %80 用时 | 99% 用时 |
                 |:-------|:--------:|:---------:|:---------:|:---------:|:---------:|
                 """);
         Consumer<ServiceCallLite.ServiceCallResult> work = r -> sb
                 .append('|').append(r.getService())
                 .append('|').append(r.getSize())
                 .append('|').append(r.getMaxTime() / 1000D).append('s')
-                .append('|').append(r80map.getOrDefault(r.getService(), 0L) / 1000D).append('s')
                 .append('|').append(Math.round(r.getAvgTime()) / 1000D).append('s')
                 .append('|').append(r.getMinTime() / 1000D).append('s')
+                .append('|').append(r1map.getOrDefault(r.getService(), 0L) / 1000D).append('s')
+                .append('|').append(r80map.getOrDefault(r.getService(), 0L) / 1000D).append('s')
+                .append('|').append(r99map.getOrDefault(r.getService(), 0L) / 1000D).append('s')
                 .append("|\n");
         result.forEach(work);
 
