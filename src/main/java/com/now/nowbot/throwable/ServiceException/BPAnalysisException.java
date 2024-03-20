ee3a3aab8e760b63c7dea99cd87c00599853cead
YumeMuzi <1340691940@qq.com>
2024-03-20 21:36:40 +0800

    BPA 尝试修复某些问题

--- src/main/java/com/now/nowbot/throwable/ServiceException/BPAnalysisException.java
+++ src/main/java/com/now/nowbot/throwable/ServiceException/BPAnalysisException.java
@@ -4,10 +4,10 @@ import com.now.nowbot.throwable.TipsException;
 
 public class BPAnalysisException extends TipsException {
     public enum Type {
-        BPA_Me_TokenExpired("您的令牌已过期，请重新授权。(!ymbind)\n除此之外，还可以试试 !ba [username]"),//查询自己_绑定失效
+        BPA_Me_TokenExpired("您的令牌已过期，或仅使用名称绑定。请重新授权。(!ymbind)\n除此之外，还可以试试 !ba [username]"),//查询自己_绑定失效
         BPA_Attr_FetchFailed("获取变化的谱面数据超时（太多了），如果你是第一次见到这条消息，请重试。"),
         BPA_Me_FetchFailed("获取你的 BPA 失败，请重试。\n如果一直出现错误，请提醒开发者。"), //玩家_获取失败
-        BPA_Player_TokenExpired("此玩家的令牌已过期，请提醒他重新授权。(!ymbind)"), //玩家_对方未绑定
+        BPA_Player_TokenExpired("此玩家的令牌已过期，或仅使用名称绑定。请提醒他重新授权。(!ymbind)"), //玩家_对方未绑定
         BPA_Player_NotFound("这是谁呀，小沐找不到他哦。"),//查询他人_未搜到玩家
         BPA_Player_FetchFailed("获取对方的 BPA 失败，请重试。\n如果一直错误请反馈开发者。"), //玩家_获取失败
         BPA_Me_NotEnoughBP("你在 %s 模式上的 BP 不足 10 个呢...\n灼热分析 EX"), //玩家_最好成绩范围错误
