e915b80f92b2f24c51ffd526679fafe385e3add9
spring <365246692@qq.com>
2024-03-21 12:01:54 +0800

    debug 查看原始消息

--- src/main/java/com/now/nowbot/service/MessageServiceImpl/PingService.java
+++ src/main/java/com/now/nowbot/service/MessageServiceImpl/PingService.java
@@ -4,6 +4,7 @@ package com.now.nowbot.service.MessageServiceImpl;
 import com.now.nowbot.config.NowbotConfig;
 import com.now.nowbot.qq.event.MessageEvent;
 import com.now.nowbot.service.MessageService;
+import com.now.nowbot.util.ContextUtil;
 import com.now.nowbot.util.DataUtil;
 import com.now.nowbot.util.Instructions;
 import io.github.humbleui.skija.*;
@@ -20,6 +21,10 @@ public class PingService implements MessageService<Matcher> {
     @Override
     public boolean isHandle(MessageEvent event, String messageText, DataValue<Matcher> data) {
         var m = Instructions.PING.matcher(messageText);
+        if (ContextUtil.getContext("isTest", Boolean.FALSE, Boolean.class)) {
+            event.getSubject().sendMessage(STR."原始消息内容:\n\{event.getRawMessage()}");
+            return false;
+        }
         if (m.find()) {
             data.setValue(m);
             return true;
@@ -31,7 +36,7 @@ public class PingService implements MessageService<Matcher> {
     public void HandleMessage(MessageEvent event, Matcher matcher) throws Throwable {
         var from = event.getSubject();
         byte[] image;
-        try (Surface surface = Surface.makeRasterN32Premul(240,240)){
+        try (Surface surface = Surface.makeRasterN32Premul(240, 240)) {
             Canvas canvas = surface.getCanvas();
 
             try {
@@ -39,22 +44,23 @@ public class PingService implements MessageService<Matcher> {
                         Path.of(NowbotConfig.EXPORT_FILE_PATH).resolve("help-ping.png")
                 );
                 var BG = Image.makeFromEncoded(file);
-                canvas.drawImage(BG,0,0);
+                canvas.drawImage(BG, 0, 0);
 
             } catch (IOException ignored) {
                 //throw new RuntimeException("ping failed cuz no BG??!");
             }
 
             Font x = new Font(DataUtil.getTorusRegular(), 60);
-            TextLine t = TextLine.make("PONG!",x);
-            canvas.drawTextLine(t,(240 - t.getWidth())/2, t.getHeight(), new Paint().setARGB(255,192,219,288));
+            TextLine t = TextLine.make("PONG!", x);
+            canvas.drawTextLine(t, (240 - t.getWidth()) / 2, t.getHeight(), new Paint().setARGB(255, 192, 219, 288));
 
             x.close();
             t.close();
-            x = new Font(DataUtil.getTorusRegular(),20);
+            x = new Font(DataUtil.getTorusRegular(), 20);
             t = TextLine.make(STR."\{System.currentTimeMillis()}ms", x);
-            canvas.drawTextLine(t,0,t.getCapHeight() + 4, new Paint().setARGB(200,255,255,255));
-            x.close();t.close();
+            canvas.drawTextLine(t, 0, t.getCapHeight() + 4, new Paint().setARGB(200, 255, 255, 255));
+            x.close();
+            t.close();
             image = surface.makeImageSnapshot().encodeToData().getBytes();
         }
         if (image != null) {
