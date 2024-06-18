0ac38e1fef936de04f6fee5d0fc4983c0e1dadac
Your Name <365246692@qq.com>
2024-06-18 18:17:05 +0800

    更新

--- src/main/java/com/now/nowbot/service/BsApiService.java
+++ src/main/java/com/now/nowbot/service/BsApiService.java
@@ -5,16 +5,20 @@ import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Optional;
+
 @Service("BsApiService")
 public class BsApiService {
     @Resource
     WebClient webClient;
-    private static final String URL = NowbotConfig.BS_API_URL;
+    private static final String           URL   = NowbotConfig.BS_API_URL;
+    private static final Optional<String> TOKEN = NowbotConfig.BS_TOKEN;
 
     public String getOsuFile(long bid) {
+        if (TOKEN.isEmpty()) throw new RuntimeException("无法权限访问");
         return webClient.get()
-                .uri(URL, b -> b.path("/").build())
-                .header("AuthorizationX", "")
+                .uri(URL, b -> b.path("/api/file/map/bg/{bid}").build(bid))
+                .header("AuthorizationX", TOKEN.get())
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
