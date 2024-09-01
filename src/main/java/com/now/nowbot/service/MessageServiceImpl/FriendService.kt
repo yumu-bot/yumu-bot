ca17b4f5dbb10203afb9522e44478b3afd2e1390
YuriMumu <1340691940@qq.com>
2024-09-01 23:56:09 +0800

    F 优化好友传递

--- src/main/java/com/now/nowbot/service/MessageServiceImpl/FriendService.kt
+++ src/main/java/com/now/nowbot/service/MessageServiceImpl/FriendService.kt
@@ -14,29 +14,34 @@ import com.now.nowbot.throwable.ServiceException.FriendException
 import com.now.nowbot.util.CmdObject
 import com.now.nowbot.util.CmdUtil
 import com.now.nowbot.util.Instruction
-import jakarta.annotation.Resource
+import com.yumu.core.extensions.isNotNull
+import java.util.*
+import java.util.concurrent.atomic.AtomicBoolean
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.util.CollectionUtils
 import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.reactive.function.client.WebClientResponseException
-import java.util.*
-import java.util.concurrent.atomic.AtomicBoolean
 
 @Service("FRIEND")
 class FriendService(
-    private val bindDao: BindDao,
-    private val userApiService: OsuUserApiService,
-    private val imageService: ImageService,
+        private val bindDao: BindDao,
+        private val userApiService: OsuUserApiService,
+        private val imageService: ImageService,
 ) : MessageService<FriendParam?> {
 
-    data class FriendParam(val offset: Int, val limit: Int, val uid: Long = 0, val name: String? = null)
+    data class FriendParam(
+            val offset: Int,
+            val limit: Int,
+            val uid: Long = 0,
+            val name: String? = null
+    )
 
     override fun isHandle(
-        event: MessageEvent,
-        messageText: String,
-        data: MessageService.DataValue<FriendParam?>
+            event: MessageEvent,
+            messageText: String,
+            data: MessageService.DataValue<FriendParam?>
     ): Boolean {
         val m = Instruction.FRIEND.matcher(messageText)
         if (!m.find()) {
@@ -60,17 +65,18 @@ class FriendService(
     @Throws(Throwable::class)
     override fun HandleMessage(event: MessageEvent, param: FriendParam?) {
         val from = event.subject
-        val binUser = try {
-            bindDao!!.getUserFromQQ(event.sender.id)
-        } catch (e: Exception) {
-            throw FriendException(FriendException.Type.FRIEND_Me_NoPermission)
-        }
+        val binUser =
+                try {
+                    bindDao.getUserFromQQ(event.sender.id)
+                } catch (e: Exception) {
+                    throw FriendException(FriendException.Type.FRIEND_Me_NoPermission)
+                }
 
         if (binUser == null) {
             throw FriendException(FriendException.Type.FRIEND_Me_NoBind)
         } else if (!binUser.isAuthorized) {
             throw FriendException(FriendException.Type.FRIEND_Me_NoPermission)
-            //无权限
+            // 无权限
         }
 
         if (param!!.uid != 0L) {
@@ -81,32 +87,58 @@ class FriendService(
         }
     }
 
-    fun checkMultiFriend(from: Contact, binUser: BinUser, user: FriendParam) {
+    fun checkMultiFriend(from: Contact, binUser: BinUser, param: FriendParam) {
         val friendList = userApiService.getFriendList(binUser)
-        val uid = user.uid
-        val friend = friendList.find { it?.userID == uid }
-        //
-        val isMulti = try {
-            val otherBinUser = bindDao?.getBindUser(uid) ?: throw Exception()
-            if (!otherBinUser.isAuthorized) {
-                throw Exception()
-            }
-            userApiService.getFriendList(otherBinUser).find { it?.userID == binUser.osuID } != null
-        } catch (ignore: Exception) {
-            // 对面没绑定不处理
-            false
-        }
 
-        val message = if (friend == null) {
-            "你并没有关注 ${user.name}"
-        } else {
-            if (isMulti) {
-                "你与 ${friend.userName} 互相关注了"
+        val message = getMutualInfo(binUser, param, friendList)
+        from.sendText(message)
+    }
+
+    fun getMutualInfo(
+            binUser: BinUser,
+            param: FriendParam,
+            friendList: MutableList<MicroUser?>
+    ): String {
+        val uid = param.uid
+        val friend = friendList.find { it?.userID == uid }
+
+        val other = bindDao.getBindUser(uid) ?: null
+
+        val isBind = other.isNotNull() && other!!.isAuthorized
+
+        val isFollowed =
+                try {
+                    if (isBind) {
+                        userApiService
+                                .getFriendList(other)
+                                .find { it?.userID == binUser.osuID }
+                                .isNotNull()
+                    } else {
+                        false
+                    }
+                } catch (ignored: Exception) {
+                    false
+                }
+
+        val isFollowing = friend.isNotNull()
+
+        return if (isFollowing) {
+            if (isFollowed) {
+                "你已经与 ${param.name} 互相成为好友了。"
+            } else if (isBind) {
+                "你已经添加了 ${param.name} 作为你的好友，但对方似乎还没有添加你。"
             } else {
-                "你已经关注 ${friend.userName} 了"
+                "你已经添加了 ${param.name} 作为你的好友，但不知道对方有没有添加你。"
+            }
+        } else {
+            if (isFollowed) {
+                "你还没有添加了 ${param.name} 作为你的好友，但对方似乎已经悄悄将你添加为他的好友。"
+            } else if (isBind) {
+                "你们暂未互相成为好友。"
+            } else {
+                "你还没有添加了 ${param.name} 作为你的好友，也不知道对方有没有添加你。"
             }
         }
-        from.sendText(message)
     }
 
     fun sendFriendList(from: Contact, binUser: BinUser, param: FriendParam) {
@@ -114,7 +146,7 @@ class FriendService(
 
         val friends: MutableList<MicroUser?> = ArrayList()
 
-        //拿到参数,默认1-24个
+        // 拿到参数,默认1-24个
         val offset = param.offset
         val limit = param.limit
         val doRandom = (offset == 0 && limit == 12)
@@ -122,7 +154,6 @@ class FriendService(
         if (limit == 0 || 100 < limit - offset) {
             throw FriendException(FriendException.Type.FRIEND_Client_ParameterOutOfBounds)
         }
-
 
         try {
             osuUser = userApiService.getPlayerInfo(binUser)
@@ -143,7 +174,7 @@ class FriendService(
 
         var index: IntArray? = null
         if (doRandom) {
-            //构造随机数组
+            // 构造随机数组
             index = IntArray(friendList.size)
             for (i in index.indices) {
                 index[i] = i
@@ -173,7 +204,8 @@ class FriendService(
             i++
         }
 
-        if (CollectionUtils.isEmpty(friends)) throw FriendException(FriendException.Type.FRIEND_Client_NoFriend)
+        if (CollectionUtils.isEmpty(friends))
+                throw FriendException(FriendException.Type.FRIEND_Client_NoFriend)
 
         try {
             val image = imageService.getPanelA1(osuUser, friends)
@@ -183,7 +215,6 @@ class FriendService(
             throw FriendException(FriendException.Type.FRIEND_Send_Error)
         }
     }
-
 
     companion object {
         private val log: Logger = LoggerFactory.getLogger(FriendService::class.java)
