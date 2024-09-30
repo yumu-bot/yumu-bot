5deec0e8743ce8018ccc29bf16c3d19493d8deeb
spring <365246692@qq.com>
2024-09-30 15:48:00 +0800

    添加日志

--- src/main/java/com/now/nowbot/service/osuApiService/OsuBeatmapMirrorApiService.java
+++ src/main/java/com/now/nowbot/service/osuApiService/OsuBeatmapMirrorApiService.java
@@ -2,8 +2,12 @@ package com.now.nowbot.service.osuApiService;
 
 import com.now.nowbot.config.NowbotConfig;
 import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.util.Optional;
 
@@ -11,6 +15,7 @@ import java.util.Optional;
 @Service("OsuBeatmapMirrorApiService")
 public class OsuBeatmapMirrorApiService {
 
+    private static final Logger log = LoggerFactory.getLogger(OsuBeatmapMirrorApiService.class);
     @Resource
     WebClient webClient;
 
@@ -18,10 +23,22 @@ public class OsuBeatmapMirrorApiService {
     private static final Optional<String> TOKEN = NowbotConfig.BEATMAP_MIRROR_TOKEN;
 
     public String getOsuFile(long bid) {
-        return webClient.get()
-                .uri(URL, b -> b.path("/api/file/map/osufile/{bid}").build(bid))
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
+        try {
+            return webClient.get()
+                    .uri(URL, b -> b.path("/api/file/map/osufile/{bid}").build(bid))
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+        } catch (Exception e) {
+            if (e instanceof WebClientResponseException re) {
+                log.error("osu file err: {}", re.getStatusText());
+                log.error(re.getResponseBodyAsString());
+            }
+            if (e instanceof WebClientRequestException re) {
+                log.error("osu file err: {}",re.getUri());
+                log.error(re.getMessage());
+            }
+            throw e;
+        }
     }
 }
