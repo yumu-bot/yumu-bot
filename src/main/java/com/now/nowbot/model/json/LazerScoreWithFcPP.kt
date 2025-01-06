04d24ab932e6e41d864063bec9d41c604e85f39f
spring <365246692@qq.com>
2025-01-06 17:53:24 +0800

    修复: BF json key 格式错误

--- src/main/java/com/now/nowbot/model/json/LazerScoreWithFcPP.kt
+++ src/main/java/com/now/nowbot/model/json/LazerScoreWithFcPP.kt
@@ -3,12 +3,20 @@ package com.now.nowbot.model.json
 import com.fasterxml.jackson.annotation.JsonAutoDetect
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.beans.BeanUtils
 
-@JsonInclude(JsonInclude.Include.NON_NULL) @JsonIgnoreProperties(ignoreUnknown = true) @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY) 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 class LazerScoreWithFcPP : LazerScore() {
+    @JsonProperty("fc_pp")
     var fcPP: Double = 0.0
+
+    @JsonProperty("index")
     var index: Int = 0
+
+    @JsonProperty("index_after")
     var indexAfter: Int = 0
 
     companion object {
