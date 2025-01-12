5af8d987577945d8d79e62c2915e0f041a55225e
spring <365246692@qq.com>
2025-01-12 22:49:53 +0800

    修复: jpa sb

--- src/main/java/com/now/nowbot/entity/RankedBeatmap.kt
+++ src/main/java/com/now/nowbot/entity/RankedBeatmap.kt
@@ -1,5 +1,6 @@
 package com.now.nowbot.entity
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import org.hibernate.annotations.Subselect
@@ -12,5 +13,6 @@ import org.hibernate.annotations.Subselect
     """)
 class RankedBeatmap(
     @Id
+    @Column(name = "bid")
     var bid: Long
 )
