0e43ca28b543bf02951d2e70887ca5b0543e0bf9
spring <365246692@qq.com>
2025-01-12 22:34:51 +0800

    修复: 视图忘记推送osu_ranked_beatmap_id

---
+++ src/main/java/com/now/nowbot/entity/RankedBeatmap.kt
@@ -0,0 +1,16 @@
+package com.now.nowbot.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import org.hibernate.annotations.Subselect
+
+@Entity(name = "osu_ranked_beatmap_id")
+@Subselect("""
+    select id as bid from osu_beatmap ob 
+    left join osu_mapset om on ob.map_id = om.map_id
+    where ob.status='ranked' or ob.status='approved';
+    """)
+class RankedBeatmap(
+    @Id
+    var bid: Long
+)
