9cbc7e0107c7b0535e0bb3deb1124f335ee9a116
spring <365246692@qq.com>
2025-03-10 20:49:39 +0800

    修复: ttm 并发调用问题

--- src/main/java/com/now/nowbot/service/messageServiceImpl/TestPPMService.kt
+++ src/main/java/com/now/nowbot/service/messageServiceImpl/TestPPMService.kt
@@ -22,18 +22,17 @@ import org.springframework.stereotype.Service
 import java.nio.charset.StandardCharsets
 import java.util.*
 import java.util.regex.Matcher
-import kotlin.math.min
 
 @Service("TEST_PPM")
 class TestPPMService(
-        private val userApiService: OsuUserApiService,
-        private val scoreApiService: OsuScoreApiService,
+    private val userApiService: OsuUserApiService,
+    private val scoreApiService: OsuScoreApiService,
 ) : MessageService<Matcher> {
 
     override fun isHandle(
-            event: MessageEvent,
-            messageText: String,
-            data: DataValue<Matcher>,
+        event: MessageEvent,
+        messageText: String,
+        data: DataValue<Matcher>,
     ): Boolean {
         val m = Instruction.TEST_PPM.matcher(messageText)
         if (m.find() && Permission.isGroupAdmin(event)) {
@@ -52,132 +51,104 @@ class TestPPMService(
             throw PPMinusException(PPMinusException.Type.PM_Test_Empty)
         }
 
-        val userMap = ConcurrentMap<String, OsuUser?>(names.size)
-        val dataMap = ConcurrentMap<String, TestPPMData?>(names.size)
-
         event.reply("正在处理玩家数据。注意需要输入玩家名，而并非玩家 ID！")
 
-        names.chunked(20).forEachIndexed { i, chunk ->
-            val actions = chunk.map {
-                return@map AsyncMethodExecutor.Supplier<Unit> {
-                    val user: OsuUser
-                    val bps: List<LazerScore>
-
-                    try {
-                        // 神奇！如果这里传 ID，会有问题！
-                        user = userApiService.getPlayerInfo(it, inputMode)
-
-                        val mode = if (OsuMode.isDefaultOrNull(inputMode)) {
-                            user.currentOsuMode
-                        } else {
-                            inputMode
-                        }
-
-                        bps = scoreApiService.getBestScores(user.userID, mode)
-
-                        val ppmData = TestPPMData()
-
-                        ppmData.init(user, bps)
-
-                        userMap.putIfAbsent(it, user)
-                        dataMap.putIfAbsent(it, ppmData)
-                    } catch (e: Exception) {
-                        // sb.append("name=").append(name).append(" not found").append('\n')
-                        e.printStackTrace()
-                        userMap.putIfAbsent(it, null)
-                        dataMap.putIfAbsent(it, null)
+        var mode: OsuMode? = null
+        val actions = names.map { name ->
+            return@map AsyncMethodExecutor.Supplier<TestPPMData?> {
+                try {
+                    val u = userApiService.getPlayerInfo(name, mode ?: inputMode)
+                    if (OsuMode.isDefaultOrNull(mode)) {
+                        mode = u.currentOsuMode
                     }
+                    val s = scoreApiService.getBestScores(u.id, mode)
+                    val ppmData = TestPPMData()
+
+                    ppmData.init(u, s)
+                    return@Supplier ppmData
+                } catch (e: Exception) {
+                    log.error("testPPM get user [$name] info error", e)
+                    return@Supplier null
                 }
             }
-
-            event.reply("当前进度：${min(20 * i, names.size)} - ${min(20 * (i + 1) - 1, names.size)} / ${names.size}")
-
-            AsyncMethodExecutor.AsyncSupplier(actions)
-
-            val await = 20 * (i + 1) < names.size
-
-            event.reply("处理完成：${min(20 * i, names.size)} - ${min(20 * (i + 1) - 1, names.size)}，${if (await) "等待 1 秒" else "即将发送"}")
-
-            if (await) {
-                // 虚拟线程等待，避免 429
-                Thread.sleep(1000L)
-            }
         }
+        val result = AsyncMethodExecutor
+            .AsyncSupplier(actions)
+            .filterNotNull()
 
         val sb = StringBuilder()
 
         sb.append("name,rank,pp,acc,level,maxcombo,totalhits,pc,pt,notfc,rawpp,ss,s,a,b,c,d,top10pp,top10acc,top10length,top10rate,mid10pp,mid10acc,mid10length,mid10rate,bottom10pp,bottom10acc,bottom10length,bottom10rate,4keypp")
             .append('\n')
 
-        for (name in names) {
-            val user = userMap[name]
-            val data = dataMap[name]
+        for (data in result) {
+            val user = data.user
 
-            if (user == null || data == null) {
-                sb.append("name=").append(name).append(" not found").append('\n')
+            if (user == null) {
+                sb.append("name=").append("unknown").append(" not found").append('\n')
                 continue
             }
 
             sb.append(user.username)
-                    .append(',')
-                    .append(user.globalRank ?: 0L)
-                    .append(',')
-                    .append(user.pp ?: 0.0)
-                    .append(',')
-                    .append(user.accuracy)
-                    .append(',')
-                    .append(user.levelCurrent)
-                    .append(',')
-                    .append(user.statistics.maxCombo)
-                    .append(',')
-                    .append(user.totalHits)
-                    .append(',')
-                    .append(user.playCount)
-                    .append(',')
-                    .append(user.playTime)
-                    .append(',')
-                    .append(data.notfc)
-                    .append(',')
-                    .append(data.rawpp)
-                    .append(',')
-                    .append(data.xx)
-                    .append(',')
-                    .append(data.xs)
-                    .append(',')
-                    .append(data.xa)
-                    .append(',')
-                    .append(data.xb)
-                    .append(',')
-                    .append(data.xc)
-                    .append(',')
-                    .append(data.xd)
-                    .append(',')
-                    .append(data.ppv0)
-                    .append(',')
-                    .append(data.accv0)
-                    .append(',')
-                    .append(data.lengv0)
-                    .append(',')
-                    .append(data.pgr0)
-                    .append(',')
-                    .append(data.ppv45)
-                    .append(',')
-                    .append(data.accv45)
-                    .append(',')
-                    .append(data.lengv45)
-                    .append(',')
-                    .append(data.pgr45)
-                    .append(',')
-                    .append(data.ppv90)
-                    .append(',')
-                    .append(data.accv45)
-                    .append(',')
-                    .append(data.lengv90)
-                    .append(',')
-                    .append(data.pgr90)
-                    .append(',')
-                    .append(user.statistics?.pP4K ?: 0.0)
-                    .append('\n')
+                .append(',')
+                .append(user.globalRank ?: 0L)
+                .append(',')
+                .append(user.pp ?: 0.0)
+                .append(',')
+                .append(user.accuracy)
+                .append(',')
+                .append(user.levelCurrent)
+                .append(',')
+                .append(user.statistics.maxCombo)
+                .append(',')
+                .append(user.totalHits)
+                .append(',')
+                .append(user.playCount)
+                .append(',')
+                .append(user.playTime)
+                .append(',')
+                .append(data.notfc)
+                .append(',')
+                .append(data.rawpp)
+                .append(',')
+                .append(data.xx)
+                .append(',')
+                .append(data.xs)
+                .append(',')
+                .append(data.xa)
+                .append(',')
+                .append(data.xb)
+                .append(',')
+                .append(data.xc)
+                .append(',')
+                .append(data.xd)
+                .append(',')
+                .append(data.ppv0)
+                .append(',')
+                .append(data.accv0)
+                .append(',')
+                .append(data.lengv0)
+                .append(',')
+                .append(data.pgr0)
+                .append(',')
+                .append(data.ppv45)
+                .append(',')
+                .append(data.accv45)
+                .append(',')
+                .append(data.lengv45)
+                .append(',')
+                .append(data.pgr45)
+                .append(',')
+                .append(data.ppv90)
+                .append(',')
+                .append(data.accv45)
+                .append(',')
+                .append(data.lengv90)
+                .append(',')
+                .append(data.pgr90)
+                .append(',')
+                .append(user.statistics?.pP4K ?: 0.0)
+                .append('\n')
         }
 
         val file = sb.toString().toByteArray(StandardCharsets.UTF_8)
@@ -209,8 +180,10 @@ class TestPPMService(
         var xs: Int = 0
         var xx: Int = 0
         var notfc: Int = 0
+        var user: OsuUser? = null
 
         fun init(user: OsuUser, bps: List<LazerScore>) {
+            this.user = user
             val bpPPs = DoubleArray(bps.size)
             for (i in bps.indices) {
                 val bp = bps[i]
@@ -223,8 +196,10 @@ class TestPPMService(
                 when (bp.rank) {
                     "XH",
                     "X" -> xx++
+
                     "SH",
                     "S" -> xs++
+
                     "A" -> xa++
                     "B" -> xb++
                     "C" -> xc++
