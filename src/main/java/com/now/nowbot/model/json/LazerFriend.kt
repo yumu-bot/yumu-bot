381a8728312487e61fe153142122e21d3f3b940a
YumeMuzi <1340691940@qq.com>
2025-05-17 16:54:42 +0800

    修复：LazerFriend

--- src/main/java/com/now/nowbot/model/json/LazerFriend.kt
+++ src/main/java/com/now/nowbot/model/json/LazerFriend.kt
@@ -5,11 +5,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 
-@JsonIgnoreProperties(ignoreUnknown = true) @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 class LazerFriend {
     @JsonProperty("target_id")
-    var targetId: Long = 0L
+    var targetID: Long = 0L
 
     @JsonProperty("relation_type")
     var relationType: String = ""
@@ -19,5 +20,7 @@ class LazerFriend {
 
     @JsonProperty("target")
     var target: MicroUser = MicroUser()
-        get() = field.apply { field.isMutual = isMutual }
+        get()  = field.apply {
+            isMutual = this@LazerFriend.isMutual
+        }
 }
