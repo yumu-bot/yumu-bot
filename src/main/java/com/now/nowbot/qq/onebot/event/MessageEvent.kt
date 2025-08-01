7405d51a84441919bd19134f031523fc33b52e8f
spring <365246692@qq.com>
2025-08-01 09:07:24 +0800

    修复: 改正消息获取

--- src/main/java/com/now/nowbot/qq/onebot/event/MessageEvent.kt
+++ src/main/java/com/now/nowbot/qq/onebot/event/MessageEvent.kt
@@ -1,6 +1,5 @@
 package com.now.nowbot.qq.onebot.event
 
-import com.mikuac.shiro.common.utils.ShiroUtils
 import com.mikuac.shiro.core.Bot
 import com.mikuac.shiro.dto.event.message.GroupMessageEvent
 import com.mikuac.shiro.dto.event.message.MessageEvent
@@ -9,25 +8,23 @@ import com.mikuac.shiro.model.ArrayMsg
 import com.now.nowbot.qq.message.*
 import com.now.nowbot.qq.onebot.contact.Contact
 import com.now.nowbot.qq.onebot.contact.Group
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import java.net.MalformedURLException
 import java.net.URI
 
-open class MessageEvent(val event: MessageEvent, bot: Bot?) : Event(bot),
+open class MessageEvent(val event: MessageEvent, bot: Bot?) : Event(bot?.selfId ?: 0),
     com.now.nowbot.qq.event.MessageEvent {
     override fun getSubject(): Contact {
         if (event is GroupMessageEvent) {
-            return Group(bot.trueBot, event.groupId)
+            return Group(bot.botID, event.groupId)
         }
-        return Contact(bot.trueBot, event.userId)
+        return Contact(bot.botID, event.userId)
     }
 
     override fun getSender(): Contact {
         return if (event is GroupMessageEvent) {
-            Group(bot.trueBot, event.sender.userId)
+            Group(bot.botID, event.sender.userId)
         } else {
-            Contact(bot.trueBot, event.userId)
+            Contact(bot.botID, event.userId)
         }
     }
 
@@ -46,31 +43,30 @@ open class MessageEvent(val event: MessageEvent, bot: Bot?) : Event(bot),
     }
 
     companion object {
-        private val log: Logger = LoggerFactory.getLogger("msg")
 
         @JvmStatic
         fun getMessageChain(msgs: List<ArrayMsg>): MessageChain {
             val msg = msgs.map {
                 return@map when (it.type) {
                     MsgTypeEnum.at -> {
-                        val qqStr = it.data["qq"]?.toString() ?: "0"
+                        val qqStr = it.getStringData("qq")
 
                         //艾特全体是 -1。扔过来的可能是 "all"
                         AtMessage(qqStr.toLongOrNull() ?: -1L)
                     }
 
                     MsgTypeEnum.text -> TextMessage(
-                        decodeArr(it.data["text"]?.toString() ?: "")
+                        it.getStringData("text")
                     )
 
                     MsgTypeEnum.reply -> ReplyMessage(
-                        (it.data["id"]?.toString() ?: "0").toLongOrNull() ?: 0L,
-                        decodeArr(it.data["text"]?.toString() ?: "")
+                        it.getLongData("id"),
+                        it.getStringData("text")
                     )
 
                     MsgTypeEnum.image -> {
                         try {
-                            ImageMessage(URI(it.data["url"]?.toString() ?: "").toURL())
+                            ImageMessage(URI(it.getStringData("url")).toURL())
                         } catch (e: MalformedURLException) {
                             TextMessage("[图片;加载异常]")
                         } catch (e: IllegalArgumentException) {
@@ -85,14 +81,6 @@ open class MessageEvent(val event: MessageEvent, bot: Bot?) : Event(bot),
             }
 
             return MessageChain(msg)
-        }
-
-        private fun decode(m: String): String {
-            return ShiroUtils.unescape(m)
-        }
-
-        private fun decodeArr(m: String): String {
-            return m
         }
     }
 }
