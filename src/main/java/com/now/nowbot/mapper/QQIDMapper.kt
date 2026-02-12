d689b6b56ab4b2c8f8ba4da9f391e3e97a3af463
YumeMuzi <77779996+YumeMuzi@users.noreply.github.com>
2026-02-13 02:57:12 +0800

    修复：无法删除权限记录

--- src/main/java/com/now/nowbot/mapper/QQIDMapper.kt
+++ src/main/java/com/now/nowbot/mapper/QQIDMapper.kt
@@ -5,17 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
-import java.beans.Transient
+import org.springframework.transaction.annotation.Transactional
 
 interface QQIDMapper : JpaRepository<QQID?, Long?>, JpaSpecificationExecutor<QQID> {
     fun getByPermissionID(permissionID: Long?): List<QQID>
 
     @Modifying
-    @Transient
+    @Transactional
     fun deleteQQIDByPermissionIDAndIsGroupAndQQ(permissionID: Long?, isGroup: Boolean?, QQ: Long?)
 
     @Modifying
-    @Transient
+    @Transactional
     fun deleteQQIDByPermissionIDAndIsGroup(permissionID: Long?, isGroup: Boolean?)
 
     @Query("select id.QQ from QQID id where id.isGroup = true and id.permissionID = :pid")
