3acd2741f6d4223eefe045d8be81b5416a893108
YumeMuzi <77779996+YumeMuzi@users.noreply.github.com>
2026-02-13 02:44:45 +0800

    修复：多余的检查导致群管理无法正常操作自己或者群聊

--- src/main/java/com/now/nowbot/service/messageServiceImpl/ServiceSwitchService.kt
+++ src/main/java/com/now/nowbot/service/messageServiceImpl/ServiceSwitchService.kt
@@ -57,11 +57,11 @@ class ServiceSwitchService(
         ),
 
         FUN(listOf("f", "fun", "amuse", "amusement", "娱乐", "玩"),
-            listOf("dice", "")
+            listOf("dice")
         ),
 
         MAIMAI(listOf("i", "mai", "maimai", "chu", "chunithm", "舞萌", "舞萌中二", "中二", "中二节奏"),
-            listOf("mai_bp", "mai_score", "mai_find", "mai_version", "mai_filter", "chu_bp", "mai_seek", "")
+            listOf("mai_audio", "mai_bp", "mai_score", "mai_dist", "mai_find", "mai_version", "mai_filter", "chu_bp", "mai_seek")
         ),
 
         AID(listOf("a", "aid", "other", "辅助"),
@@ -142,7 +142,7 @@ class ServiceSwitchService(
             } + target.second
         }
 
-        val services = getServices(event, targetStr, matcher.group(FLAG_SERVICE), operate, level)
+        val services = getServices(event, targetStr, matcher.group(FLAG_SERVICE), operate)
 
         if (operate == LIST) {
             return SwitchParam(services, LIST, target?.first, target?.second)
@@ -231,7 +231,7 @@ class ServiceSwitchService(
         }
     }
 
-    private fun getServices(event: MessageEvent, target: String, input: String?, operate: Operate, level: Level): List<String> {
+    private fun getServices(event: MessageEvent, target: String, input: String?, operate: Operate): List<String> {
         if (operate == LIST) return listOf()
 
         val ii = (input ?: "").trim().replace("\\s+".toRegex(), "_").uppercase()
@@ -246,14 +246,13 @@ class ServiceSwitchService(
 
         if (ii.isEmpty()) {
             // 全局操作模式
-            if (level != Level.SUPER) {
-                throw PermissionException.DeniedException.BelowSuperAdministrator()
-            }
 
             val receipt = event.reply("""
                 您确定要${does}所有服务吗？回复 OK 确认。
                 操作对象：${target}
                 操作服务：所有
+                
+                如果不想${does}所有服务，也可以尝试按组来${does}。
             """.trimIndent())
 
             val lock = ASyncMessageUtil.getLock(event, 30 * 1000L)
@@ -288,10 +287,11 @@ class ServiceSwitchService(
                 您可以按类别来操作服务。
                 
                 可用的类别：
+                
             """.trimIndent())
 
             confirm.append(ServiceType.entries.joinToString("\n") {
-                "${it.name}: ${it.services.joinToString(", ")}"
+                "${it.name}:\n${it.services.joinToString(", ")}"
             })
 
             throw TipsException(confirm.toString())
@@ -483,230 +483,4 @@ class ServiceSwitchService(
     companion object {
         private val log = LoggerFactory.getLogger(ServiceCallStatistic::class.java)
     }
-
-    /*
-
-    data class SwitchParam(val groupID: Long, val serviceName: String?, val operation: Operation)
-
-    enum class Operation {
-        REVIEW,
-        ON,
-        OFF,
-    }
-
-    @Throws(Throwable::class)
-    override fun isHandle(
-        event: MessageEvent,
-        messageText: String,
-        data: DataValue<SwitchParam>
-    ): Boolean {
-        val m = Instruction.SERVICE_SWITCH_ON.matcher(messageText)
-        return false
-
-        if (!Permission.isSuperAdmin(event.sender.id)) {
-            throw PermissionException.DeniedException.BelowSuperAdministrator()
-        }
-
-        val service = m.group("service") ?: ""
-        val operate = m.group("operate") ?: ""
-
-        // var o = Pattern.compile("(black|white)?list|on|off|start|close|[bkwlofsc]+");
-        val groupStr: String = m.group(FLAG_QQ_GROUP) ?: ""
-
-        if (service.isNotBlank()) {
-            if (operate.isNotBlank()) {
-                if (groupStr.isNotBlank()) {
-                    data.value = SwitchParam(
-                        groupStr.toLong(),
-                        service.uppercase(Locale.getDefault()),
-                        getOperation(operate)
-                    )
-
-                } else {
-                    data.value = SwitchParam(
-                        -1L,
-                        service.uppercase(Locale.getDefault()),
-                        getOperation(operate)
-                    )
-                }
-            } else {
-                if (groupStr.isNotBlank()) {
-                    throw ServiceSwitchException(ServiceSwitchException.Type.SW_Parameter_OnlyGroup)
-                } else {
-                    val op = getOperation(service.uppercase(Locale.getDefault()))
-                    if (op != Operation.REVIEW) {
-                        throw ServiceSwitchException(ServiceSwitchException.Type.SW_Service_Missing)
-                    }
-                    data.value = SwitchParam(-1L, null, Operation.REVIEW)
-                }
-            }
-        } else {
-            if (groupStr.isNotBlank()) {
-                throw ServiceSwitchException(ServiceSwitchException.Type.SW_Parameter_OnlyGroup)
-            } else {
-                data.value = SwitchParam(-1L, null, Operation.REVIEW)
-                // throw ServiceSwitchException(ServiceSwitchException.Type.SW_Instructions)
-            }
-        }
-
-        return true
-    }
-
-    // @CheckPermission(isSuperAdmin = true)
-    @Throws(Throwable::class)
-    override fun handleMessage(event: MessageEvent, param: SwitchParam): ServiceCallStatistic? {
-        val service = param.serviceName!!
-        val group = param.groupID
-
-        when (param.operation) {
-            Operation.ON -> {
-                try {
-                    when (group) {
-                        -1L -> {
-                            controller.serviceSwitch(service, true)
-                            event.reply("已启动 $service 服务")
-                        }
-                        0L -> {
-                            event.reply("已全面解禁 $service 服务（并未修好）")
-                        }
-                        else -> {
-                            controller.unblockGroup(service, group)
-                            event.reply("已解禁群聊 $group 的 $service 服务")
-                        }
-                    }
-                } catch (e: TipsRuntimeException) {
-                    throw ServiceSwitchException(
-                        ServiceSwitchException.Type.SW_Service_RemoveNotExists, service
-                    )
-                } catch (e: RuntimeException) {
-                    throw ServiceSwitchException(
-                        ServiceSwitchException.Type.SW_Service_NotFound, service
-                    )
-                }
-            }
-
-            Operation.OFF -> {
-                try {
-                    when (group) {
-                        -1L -> {
-                            controller.serviceSwitch(service, false)
-                            event.reply("已关闭 $service 服务")
-                        }
-                        0L -> {
-                            event.reply("已全面禁止 $service 服务（并未修好）")
-                        }
-                        else -> {
-                            controller.blockGroup(service, group)
-                            event.reply("已禁止群聊 $group 的 $service 服务")
-                        }
-                    }
-                } catch (e: TipsRuntimeException) {
-                    throw ServiceSwitchException(
-                        ServiceSwitchException.Type.SW_Service_AddExists, service
-                    )
-                } catch (e: RuntimeException) {
-                    throw ServiceSwitchException(
-                        ServiceSwitchException.Type.SW_Service_NotFound, service
-                    )
-                }
-            }
-
-            Operation.REVIEW -> {
-                val md = serviceListMarkdown
-                try {
-                    val image = imageService.getPanelA6(md, "switch")
-                    event.reply(image)
-                } catch (e: HttpServerErrorException.InternalServerError) {
-                    throw ServiceSwitchException(ServiceSwitchException.Type.SW_Render_Failed)
-                } catch (e: WebClientResponseException.InternalServerError) {
-                    throw ServiceSwitchException(ServiceSwitchException.Type.SW_Render_Failed)
-                }
-            }
-        }
-        return ServiceCallStatistic.building(event)
-    }
-
-    private val serviceListMarkdown: String
-        get() {
-            // 这里的状态很复杂, 每个服务有三个id list(群, qq, ignore的群)
-            val data = controller.queryAllBlock()!!
-            val service1 = data.first()!!
-            // 是否为开启状态
-            service1.enable
-
-            // 群黑名单
-            service1.groups
-
-            // 用户黑名单
-            service1.users
-
-            // ignore
-            service1.ignores
-
-            // 另外作用在全局服务的状态是第一个, 可以通过下面来判断
-            // service1.name == PermissionImplement.GLOBAL_PERMISSION
-            // 或者直接获取
-            // controller!!.queryGlobal()
-
-            val sb = StringBuilder()
-            sb.append("## 服务：开关状态\n")
-
-            sb.append(
-                """
-
-                要控制开关，请按 !sw service operate 的语法输入！
-
-                | 状态 | 服务名 | 无法使用的群聊 |
-                | :-: | :-- | :-- |
-
-                """
-                    .trimIndent()
-            )
-
-            val closedServices = Permission.getClosedService()
-
-            for (name in Permission.getAllService().sorted()) {
-                var isClosed = false
-
-                for (it in closedServices) {
-                    if (name.equals(it, true)) {
-                        isClosed = true
-                        break
-                    }
-                }
-
-                sb.append("| ")
-                    .append(if (isClosed) "X" else "O")
-                    .append(" | ")
-                    .append(name)
-                    .append(" | ")
-                    .append("-") // 114514, 1919810
-                    .append(" |\n")
-            }
-
-            return sb.toString()
-        }
-
-    companion object {
-        const val SWITCH_SERVICE_NAME = "SWITCH"
-        private fun getOperation(str: String): Operation {
-            return when (str) {
-                "on",
-                "start",
-                "o",
-                "s" -> Operation.ON
-
-                "off",
-                "close",
-                "end",
-                "f",
-                "c",
-                "e" -> Operation.OFF
-
-                else -> Operation.REVIEW
-            }
-        }
-    }
-
-     */
 }
