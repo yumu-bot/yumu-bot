818ce87e9017558db7468fae601c42fa6385a509
YumeMuzi <1340691940@qq.com>
2026-03-09 16:12:38 +0800

    修复：cacheable

--- src/main/java/com/now/nowbot/service/osuApiService/impl/BeatmapApiImpl.kt
+++ src/main/java/com/now/nowbot/service/osuApiService/impl/BeatmapApiImpl.kt
@@ -24,6 +24,7 @@ import okhttp3.internal.toImmutableList
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.util.DigestUtils
