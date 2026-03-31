378dbdb7898eb32258592d45b8e4888166796f3a
YumeMuzi <77779996+YumeMuzi@users.noreply.github.com>
2026-04-01 00:19:02 +0800

    愚人节：禁言撤销

--- src/main/java/com/now/nowbot/service/NewbieRestrictService.kt
+++ src/main/java/com/now/nowbot/service/NewbieRestrictService.kt
@@ -217,7 +217,7 @@ class NewbieRestrictService(
          * 获取禁言时长（分钟）
          */
         private fun getSilence(star: Double): Long {
-            return if (star <= 5.71) {
+            return if (star <= 10) {
                 // 未超星
                 0L
             } else if (star < 6.0) {
