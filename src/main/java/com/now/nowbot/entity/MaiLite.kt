213008af6ee453c57d2a9fedde0a93f7816eed9e
YumeMuzi <77779996+YumeMuzi@users.noreply.github.com>
2026-07-05 08:40:48 +0800

    修改：优化舞萌、中二数据查询

--- src/main/java/com/now/nowbot/entity/MaiLite.kt
+++ src/main/java/com/now/nowbot/entity/MaiLite.kt
@@ -10,7 +10,7 @@ import kotlin.math.roundToInt
 
 @Entity(name = "maimai_song")
 // @Table(indexes = [Index(name = "mai_title_query", columnList = "query_text")])
-// CREATE INDEX mai_title_trgm ON maimai_song USING gin (query_text gin_trgm_ops);
+// CREATE INDEX mai_title_trgm ON maimai_song USING gin (upper(query_text) gin_trgm_ops);
 class MaiSongLite(
     @Id
     var songID: Int? = null,
@@ -361,7 +361,7 @@ class ChuAliasLite(
 @Entity
 @Table(name = "chunithm_song")
 // @Table(indexes = [Index(name = "chu_title_query", columnList = "query_text")])
-// CREATE INDEX chu_title_trgm ON chunithm_song USING gin (query_text gin_trgm_ops);
+// CREATE INDEX chu_title_trgm ON chunithm_song USING gin (upper(query_text) gin_trgm_ops);
 class ChuSongLite(
     @Id
     @Column(columnDefinition = "integer")
@@ -494,7 +494,7 @@ class ChuChartLite(
 @Table(
     name = "lx_maimai_song",
     // indexes = [Index(name = "lx_mai_title_query", columnList = "query_text")]
-    // CREATE INDEX lx_mai_title_trgm ON lx_maimai_song USING gin (query_text gin_trgm_ops);
+    // CREATE INDEX lx_mai_title_trgm ON lx_maimai_song USING gin (upper(query_text) gin_trgm_ops);
 )
 class LxMaiSongLite {
     @Id
