36258a5d88b9a8e1a1c60635dfafce907928cf41
YumeMuzi <77779996+YumeMuzi@users.noreply.github.com>
2026-07-26 19:44:30 +0800

    修复：编译错误

--- src/main/java/com/now/nowbot/mapper/OsuGroupConfigRepository.kt
+++ src/main/java/com/now/nowbot/mapper/OsuGroupConfigRepository.kt
@@ -2,8 +2,9 @@ package com.now.nowbot.mapper
 
 import com.now.nowbot.entity.OsuGroupConfigLite
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
+import org.springframework.data.jpa.repository.Query
 
 interface OsuGroupConfigRepository : JpaRepository<OsuGroupConfigLite, Long> {
-    fun findByGroupId(id: Long): Optional<OsuGroupConfigLite>
+    @Query("SELECT g FROM OsuGroupConfigLite g WHERE g.groupID = :groupID")
+    fun findByGroupID(groupID: Long): OsuGroupConfigLite?
 }
