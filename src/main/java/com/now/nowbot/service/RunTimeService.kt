7583b88f5b5983b4751ab5dbfe3f6794e4078dde
YumeMuzi <77779996+YumeMuzi@users.noreply.github.com>
2026-09-13 08:58:02 +0800

    重启更新

--- src/main/java/com/now/nowbot/service/RunTimeService.kt
+++ src/main/java/com/now/nowbot/service/RunTimeService.kt
@@ -42,7 +42,7 @@ class RunTimeService(
     //@Scheduled(cron = "0(秒) 0(分) 0(时) *(日) *(月) *(周) *(年,可选)")  '/'步进
 
     // 每 1 分钟刷新 4 个人的 token
-    // @Scheduled(cron = "0,15,30,45 * 0-1,6-23 * * *")
+    @Scheduled(cron = "0,15,30,45 * 0-1,6-23 * * *")
     fun refreshToken() {
         bindDao.refreshOldUserToken(userApiService)
     }
